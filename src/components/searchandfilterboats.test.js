84a0ac8adaf4704c38dc550f2747ad042ea5b0c8
jcable <julian.cable@yahoo.com>
2020-05-04 07:17:12 +0100

    start of improving test

--- src/components/searchandfilterboats.test.js
+++ src/components/searchandfilterboats.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MockedProvider } from "@apollo/react-testing";
+import gql from 'graphql-tag';
 import SearchAndFilterBoats from './searchandfilterboats';
+import { sampleBoatNames } from '../mock/sampledata';
+
+const mocks = [
+  {
+    request: {
+      query: gql`{
+        boat{name previous_names}
+        designer{name}
+        builder{name}
+        rig_type {name}
+        sail_type{name}
+        design_class {name}
+        generic_type{name}
+        construction_material{name}        
+    }`,
+      variables: { }
+    },
+    result: {
+      data: {
+        boat: sampleBoatNames
+      }
+    }
+  },
+];
 
 test('renders learn react link', () => {
-  const { getAllByText } = render(<SearchAndFilterBoats />);
-  const wanted = getAllByText(/Boat/);
+  const { getAllByText } = render(
+    <MockedProvider mocks={mocks}>
+      <SearchAndFilterBoats />
+    </MockedProvider>
+  );
+  const wanted = getAllByText(/Loading.../);
   expect(wanted[0]).toBeInTheDocument();
 });
