2657e8fe7b8ec71934525ed5c92c3718caf3b357
jcable <julian.cable@yahoo.com>
2020-05-08 20:58:00 +0100

    Updates

---
+++ precache-manifest.38b4d4bba27b0071d126c11b69edfcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f076866343cf66ede87507beac7be1",
+    "url": "/oga-boatregister-frontend/index.html"
+  },
+  {
+    "revision": "9d42303af4bae778a642",
+    "url": "/oga-boatregister-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6783e693cb1571330903",
+    "url": "/oga-boatregister-frontend/static/js/2.4aecf8bc.chunk.js"
+  },
+  {
+    "revision": "1ce4ddec20f4eb7b282149251c3f65df",
+    "url": "/oga-boatregister-frontend/static/js/2.4aecf8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d42303af4bae778a642",
+    "url": "/oga-boatregister-frontend/static/js/main.90e72691.chunk.js"
+  },
+  {
+    "revision": "2a4053a61e83a0d3699f",
+    "url": "/oga-boatregister-frontend/static/js/runtime-main.ff857cf3.js"
+  }
+]);
