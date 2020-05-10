6f66d0157d609612dd70950944b5243ffcbfdc81
jcable <julian.cable@yahoo.com>
2020-05-10 18:44:13 +0100

    init

--- README.md
+++
@@ -1,3 +0,0 @@
-# oga-boat-register-frontend
-React/MaterialUI/Apollo/GraphQL viewer for the OGA Boat Register
-![Node.js CI](https://github.com/jcable/oga-boat-register-frontend/workflows/Node.js%20CI/badge.svg)
--- src/components/main.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Main from './main';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Main />);
-  const wanted = getByText(/Main/);
-  expect(wanted).toBeInTheDocument();
-});
