09dc4d29567a947c4f927a2fbf8acdb404cf5428
jcable <julian.cable@yahoo.com>
2020-05-17 12:09:42 +0100

    Updates

--- precache-manifest.1449e3bbded1a3ab408eda1c4cd1b29a.js
+++ precache-manifest.1449e3bbded1a3ab408eda1c4cd1b29a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f82bf419ab8154f462ad3d480cf1c1d",
+    "revision": "ae205a740d7a42ff5ecf039245d23864",
     "url": "/boats/index.html"
   },
   {
-    "revision": "ba26f036ed3ccd09a89b",
+    "revision": "314393f0268a4f54f664",
     "url": "/boats/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "ae8ab45313725eaf5e1d",
-    "url": "/boats/static/js/2.db41768b.chunk.js"
+    "revision": "718a3b16d880ebacedf8",
+    "url": "/boats/static/js/2.42eb6e0b.chunk.js"
   },
   {
     "revision": "095b8020e69f665e9fbb277538aa6d93",
-    "url": "/boats/static/js/2.db41768b.chunk.js.LICENSE.txt"
+    "url": "/boats/static/js/2.42eb6e0b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ba26f036ed3ccd09a89b",
-    "url": "/boats/static/js/main.d8c943c7.chunk.js"
+    "revision": "314393f0268a4f54f664",
+    "url": "/boats/static/js/main.52f32e32.chunk.js"
   },
   {
     "revision": "7db0c125a4ba03974011",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/boats/precache-manifest.7d2fe0b0d6ff33c113e2d34221d8d25f.js"
+  "/boats/precache-manifest.1449e3bbded1a3ab408eda1c4cd1b29a.js"
 );
 
 self.addEventListener('message', (event) => {
