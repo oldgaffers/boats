addc00d2ab43eefd8fb4ef69d909a03a57dfe2ae
jcable <julian.cable@yahoo.com>
2020-05-17 23:25:39 +0100

    iframe

--- src/components/tech.js
+++ src/components/tech.js
@@ -1,47 +1,13 @@
 import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import LeftMenu from './leftmenu';
 import DrawerController from './drawercontroller';
 import { makeStyles } from '@material-ui/core/styles';
 import Iframe from 'react-iframe'
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '100%',
-  },
-  cardMediaSmall: {
-    paddingTop: '56.25%',
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
   root: {
     display: 'flex',
   },
@@ -78,10 +44,14 @@ function Tech({ window }) {
         container={container}
       />
       <Paper>
-        <Grid container direction="row">
+        <Grid className={classes.root} container direction="row">
           <DrawerController onClick={handleDrawerToggle} />
         </Grid>
-        <Iframe url="/tech.html" height="100%" width="100%" />
+        <Grid container direction="row">
+          <Grid item xs={12}>
+            <Iframe url="https://oldgaffers.github.io/tech.html" height="100%" width="100%" />
+          </Grid>
+        </Grid>
      </Paper>
     </div>
   );
