82a4bd22400fd21a273e674752f3d87995456c47
jcable <julian.cable@yahoo.com>
2020-05-17 20:36:10 +0100

    Updates

---
+++ precache-manifest.1fb149e10ca8f098b22cf3ca38245203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11810618d7a4f80383bc3c7f356363cb",
+    "url": "/boats/index.html"
+  },
+  {
+    "revision": "4cade72a346069ebab9e",
+    "url": "/boats/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1581c985942df2c332d1",
+    "url": "/boats/static/js/2.fd9581b7.chunk.js"
+  },
+  {
+    "revision": "dee416ddf3bba67f4b0fdf95283ca084",
+    "url": "/boats/static/js/2.fd9581b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cade72a346069ebab9e",
+    "url": "/boats/static/js/main.f2292aef.chunk.js"
+  },
+  {
+    "revision": "7db0c125a4ba03974011",
+    "url": "/boats/static/js/runtime-main.189a47b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/boats/precache-manifest.ab27e00156b2add93b7daf6e5cfa9b3b.js"
+  "/boats/precache-manifest.1fb149e10ca8f098b22cf3ca38245203.js"
 );
 
 self.addEventListener('message', (event) => {
