2e99e28beaf17ae8e432b1c7ffc4473e39cbf2da
jcable <julian.cable@yahoo.com>
2020-05-20 07:59:49 +0100

    #23 for testing

--- src/components/enquiry.js
+++ src/components/enquiry.js
@@ -12,96 +12,107 @@ import { useMutation } from '@apollo/react-hooks';
 
 const ADD_ENQUIRY = gql`
   mutation AddEnquiry($id: uuid!, $email: String!, $type: enquiry_type_enum!) {
-    insert_enquiry(objects: {boat: $id, email: $email, type: $type}) {
-    returning {
-      id
+    insert_enquiry(objects: { boat: $id, email: $email, type: $type }) {
+      returning {
+        id
+      }
     }
   }
-}
 `;
 
 const DELETE_ENQUIRY = gql`
-mutation DeleteEnquiry($id: uuid!) {
-    delete_enquiry(where: {id: {_eq: $id}}) {affected_rows}
+  mutation DeleteEnquiry($id: uuid!) {
+    delete_enquiry(where: { id: { _eq: $id } }) {
+      affected_rows
+    }
   }
 `;
 
-export default function Enquiry({ id, classes, buttons=[
-{ label: 'Make contact with the owner', key: 'general' },
-{ label: 'Add pictures / text', key: 'addinfo' },
-{ label: 'Request a handicap', key: 'handicap' },
-],
-label='enter an email and click one of the options below if you want to be contacted about this boat'
+export default function Enquiry({
+  id,
+  classes,
+  buttons = [
+    { label: 'Make contact with the owner', key: 'general' },
+    { label: 'Add pictures / text', key: 'addinfo' },
+    { label: 'Request a handicap', key: 'handicap' },
+  ],
+  label = 'enter an email and click one of the options below if you want to be contacted about this boat',
 }) {
+  const [email, setEmail] = useState();
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [recallSnackBarOpen, setRecallSnackBarOpen] = useState(false);
+  const [addEnquiry, result] = useMutation(ADD_ENQUIRY);
+  const [deleteEnquiry] = useMutation(DELETE_ENQUIRY);
 
-    const [enabled, setEnabled] = useState(false);
-    const [snackBarOpen, setSnackBarOpen] = useState(false);
-    const [recallSnackBarOpen, setRecallSnackBarOpen] = useState(false);
-    const [addEnquiry, result] = useMutation(ADD_ENQUIRY);
-    const [deleteEnquiry] = useMutation(DELETE_ENQUIRY);
-    let email = "";
+  function recallEnquiry() {
+    setSnackBarOpen(false);
+    const id = result.data.insert_enquiry.returning[0].id;
+    console.log(id);
+    deleteEnquiry({ variables: { id } });
+    setRecallSnackBarOpen(true);
+  }
 
-    function recallEnquiry() {
-        setSnackBarOpen(false);
-        const id = result.data.insert_enquiry.returning[0].id;
-        console.log(id);
-        deleteEnquiry({ variables: { id } });
-        setRecallSnackBarOpen(true);
+  const handleSnackBarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
     }
+    setSnackBarOpen(false);
+  };
 
-    const handleSnackBarClose = (event, reason) => {
-      if (reason === 'clickaway') {
+  function handleRecallSnackBarClose() {
+    setRecallSnackBarOpen(false);
+  }
+
+  function handleEmail(event) {
+    if (event.target.value) {
+      if (event.target.value.trim() !== '' && event.target.checkValidity()) {
+        setEmail(event.target.value);
         return;
       }
-      setSnackBarOpen(false);
-    };
-
-    function handleRecallSnackBarClose() {
-        setRecallSnackBarOpen(false);
     }
+    setEmail(undefined);
+  }
 
-    function handleEmail(event) {
-        if (event.target.value) {
-            if (event.target.value.trim() !== '' && event.target.checkValidity()) {
-                email = event.target.value;
-                setEnabled(true);
-                return;
-            }
-        }
-        setEnabled(false);
-    }
+  function handleClick(event, key) {
+    addEnquiry({ variables: { type: key, id, email } });
+    setSnackBarOpen(true);
+  }
 
-    function handleClick(event, key) {
-        addEnquiry({ variables: { type: key, id, email } });
-        setSnackBarOpen(true);
-    }
-
-    return (
-        <Grid container direction="column">
-           <Grid container direction="row" alignItems="flex-end">
-              <Grid item xs={10}>
-              <Typography>{label}</Typography>
-              </Grid>
-              </Grid>
-                  <Grid container direction="row" alignItems="flex-end">
-              <Grid item xs={2}>
-                  <TextField onChange={handleEmail} fullWidth={true} type="email" id="sender-email" label="your email" />
-              </Grid>
-              <Grid item xs={8}>
-            {buttons.map((button) => (
-                <Button size="small" 
-                    key={button.key}
-                    variant="contained"
-                    color="primary"
-                    disabled={!enabled}
-                    className={classes.button}
-                    endIcon={<Icon>send</Icon>}
-                    onClick={(e) => handleClick(e, button.key)}
-                >{button.label}</Button>
-            ))}                  
-              </Grid>
-          </Grid>
-          <Snackbar
+  return (
+    <Grid container direction="column">
+      <Grid container direction="row" alignItems="flex-end">
+        <Grid item xs={10}>
+          <Typography>{label}</Typography>
+        </Grid>
+      </Grid>
+      <Grid container direction="row" alignItems="flex-end">
+        <Grid item xs={2}>
+          <TextField
+            onChange={handleEmail}
+            fullWidth={true}
+            type="email"
+            id="sender-email"
+            label="your email"
+          />
+        </Grid>
+        <Grid item xs={8}>
+          {buttons.map((button) => (
+            <Button
+              size="small"
+              key={button.key}
+              variant="contained"
+              color="primary"
+              disabled={!email}
+              className={classes.button}
+              endIcon={<Icon>send</Icon>}
+              onClick={(e) => handleClick(e, button.key)}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </Grid>
+      </Grid>
+      <Snackbar
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         open={snackBarOpen}
         autoHideDuration={6000}
@@ -109,8 +120,15 @@ label='enter an email and click one of the options below if you want to be conta
         message="Enquiry Sent"
         action={
           <React.Fragment>
-            <Button color="secondary" size="small" onClick={recallEnquiry}>RECALL</Button>
-            <IconButton size="small" aria-label="close" color="inherit" onClick={handleSnackBarClose}>
+            <Button color="secondary" size="small" onClick={recallEnquiry}>
+              RECALL
+            </Button>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleSnackBarClose}
+            >
               <CloseIcon fontSize="small" />
             </IconButton>
           </React.Fragment>
@@ -124,5 +142,5 @@ label='enter an email and click one of the options below if you want to be conta
         message="It never happened"
       />
     </Grid>
-    );    
+  );
 }
