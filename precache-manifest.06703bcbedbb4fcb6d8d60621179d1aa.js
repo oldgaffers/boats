8341d2e8367cb9327c10b1f77a5831a7a2df4179
jcable <julian.cable@yahoo.com>
2020-06-14 12:37:01 +0100

    Updates

---
+++ precache-manifest.06703bcbedbb4fcb6d8d60621179d1aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891fd16db860ce140485016b069dbe26",
+    "url": "/boat_register/index.html"
+  },
+  {
+    "revision": "7d32af4035a0cde4e1b0",
+    "url": "/boat_register/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a52acb0e5847b1fcffdd",
+    "url": "/boat_register/static/js/2.2047860b.chunk.js"
+  },
+  {
+    "revision": "dee416ddf3bba67f4b0fdf95283ca084",
+    "url": "/boat_register/static/js/2.2047860b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d32af4035a0cde4e1b0",
+    "url": "/boat_register/static/js/main.4b9b6e13.chunk.js"
+  },
+  {
+    "revision": "b72b99f1078ba9123dae",
+    "url": "/boat_register/static/js/runtime-main.80b1d307.js"
+  }
+]);
