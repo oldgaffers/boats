11f1a61effc962970b76d424b0f4fbdccf38a308
jcable <julian.cable@yahoo.com>
2020-11-16 21:41:35 +0000

    new text

--- src/components/boat.js
+++ src/components/boat.js
@@ -25,9 +25,9 @@ import SmugMugGallery from './smugmuggallery';
 import Enquiry from './enquiry';
 import { feet, price } from '../util/format';
 // import Upload from './Upload';
-// import FBShare from './fbshare';
+import FBShare from './fbshare';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-import ReactFBLike from 'react-fb-like';
+//import ReactFBLike from 'react-fb-like';
 import {Helmet} from "react-helmet";
 import References from './references';
 
@@ -366,7 +366,7 @@ const engine = {
                   </CopyToClipboard>
                 </div>
                 <div>
-                  <ReactFBLike href={link} language="en_GB" appId="644249802921642" version="v2.12" />
+                  <FBShare link={link}/>
                 </div>
                 </Paper>
             </Grid>
--- src/components/boatsforsaleintro.js
+++ src/components/boatsforsaleintro.js
@@ -5,25 +5,14 @@ export default function BoatsForSaleIntro() {
     return (
         <>
         <Typography variant="body1">
-          We have hundreds of boats with pictures, and many more waiting for
-          pictures and more information.
-        </Typography>
-        <Typography variant="body1">
-          Filter the list using the options below, and then click the 'More' button
-          to see all the pictures and information we have for that boat.
-          </Typography>
-        <Typography variant="body1">
-            Have a boat and can't find it here? Fill in our{' '}
-            <a href="https://form.jotform.com/jfbcable/new-boat">form</a>
-             &nbsp;and we will add it.
+        We have lots listed for sale! 
+        Filter the list using the options below, 
+        and then click the 'More' button to see all the pictures and information 
+        we have for that boat.
             </Typography>
         <Typography variant="body1">
-            You can also use the form to suggest a boat whether you own it or
-            not.
-            </Typography>
-        <Typography variant="body1">
-            You can submit pictures, additions, and corrections to boats, or
-            contact the owner from the boat's detail page.
+            Interested in a boat? Use the contact button on the boat's detail page and our editors
+            will contact the owner for you.
             </Typography>
         <Typography variant="body1">
             Members can use the register to advertise their boat for sale. The
