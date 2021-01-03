63a285682c09cebfa58db03355002015ed79dfa0
jcable <julian.cable@yahoo.com>
2021-01-03 08:00:07 +0000

    tidy

--- src/components/editbutton.js
+++ src/components/editbutton.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
-//import Dialog from '@material-ui/core/Dialog';
-//import DialogTitle from '@material-ui/core/DialogTitle';
-//import EditBoat from './forms/editboat';
 import UpdateBoatDialog from './UpdateBoatDialog';
 
 export default function EditButton({ classes, key, disabled, boat }) {
@@ -26,14 +23,6 @@ export default function EditButton({ classes, key, disabled, boat }) {
             I have edits for this boat
       </Button>
       <UpdateBoatDialog boat={boat} onClose={handleClose} open={open} />
-      {/*
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title"
-      fullWidth maxWidth="200px" style={{minHeight: "50vh", maxHeight: "60vh" }}
-      >
-      <DialogTitle id="form-dialog-title">Edit {boat.name} ({boat.oga_no})</DialogTitle>
-        <EditBoat boat={boat} onClose={handleClose} />
-      </Dialog>
-      */}
     </div>
   );
 }
