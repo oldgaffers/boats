f02bf4ece1e76eadcdaa7399ddc3e4bbe04e0519
jcable <julian.cable@yahoo.com>
2021-01-09 09:16:58 +0000

    override oga.org.uk styles

--- src/components/detailbar.js
+++ src/components/detailbar.js
@@ -2,10 +2,16 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: { height: 'auto', padding: 0 },
+}));
 
 export default function DetailBar({ onChange, value, panes }) {
+  const classes = useStyles();
   return (
-    <AppBar position="static" color="inherit">
+    <AppBar position="static" color="inherit" classes={classes}>
         <Tabs
         onChange={onChange}
             value={value}
