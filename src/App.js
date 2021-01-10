8260ad868aeaca1e0c5f2c2892efa9cf249d7fa6
jcable <julian.cable@yahoo.com>
2021-01-10 07:24:47 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -3,24 +3,10 @@ import ApolloClient from "apollo-client";
 import { ApolloProvider } from '@apollo/react-hooks';
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { Router } from "@reach/router"
 import './App.css';
 import BrowseBoats from './components/browseboats';
 import Boat from './components/boat';
-import Designers from './components/designers';
-import Builders from './components/builders';
-import Fleets from './components/fleets';
-import About from './components/about';
-import Support from './components/support';
-import Tech from './components/tech';
-import Editors from './components/editors';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  useLocation,
-  useHistory,
-} from "react-router-dom";
 import { usePicklists } from './util/picklists';
 
 const client = new ApolloClient({
@@ -37,58 +23,26 @@ const defaultState = {
   filters: { sale: false }, 
 };
 
-function FourOhFour() {
-
-  const location = useLocation();
-  const history = useHistory();
-
+function Register({sale, state}) {
   const { loading, error, data } = usePicklists();
 
   if (loading) return (<p>Loading...</p>);
   if (error) return (<p>Error :(SearchAndFilterBoats)</p>);
 
-  const { search, pathname, state } = location;
+  const initialState = defaultState;
+  initialState.filters.sale = true;
+  initialState.sortField = 'price';
+  initialState.sortDirection = 'desc';
 
-  const sale = pathname === '/boat_register/boats_for_sale/boats_for_sale.html';
- 
-  if (!state) {
-    console.log('app state falsy, setting defaults');
-    const initialState = defaultState;
-    if (sale) {
-      initialState.filters.sale = true;
-      initialState.sortField = 'price';
-      initialState.sortDirection = 'desc';
-    }
-    history.replace('/', initialState);
-  }
-
-  const handlePageSizeChange = (a) => {
-    console.log('FourOhFour page size change', a);
-    history.replace('/', { ...state, boatsPerPage: a });
+  const handlePageSizeChange = (bpp) => {
   };
 
   const handleSortChange = (field, dir) => {
-    console.log('FourOhFour sortchange', field, dir);
-    history.replace('/', { ...state, sortField: field, sortDirection: dir });
   }
 
-  const handleFilterChange = (f) => {
-    console.log('FourOhFour setFilters', f);
-    history.replace('/', { ...state, filters: f });
+  const handleFilterChange = (filters) => {
   }
 
-  if (search !== '') {
-    const params = new URLSearchParams(search);
-    const path = params.get('p');
-    console.log('FourOhFour path', path);
-    const oga_no = params.get('oga_no');
-    if (oga_no) {
-      return (<Redirect to={`/boat/${oga_no}`} />)
-    }
-    if(path) {
-      return (<Redirect to={path} />)
-    }
-  }
   return (
   <BrowseBoats
     sale={sale} pickers={data} state={state} defaultState={defaultState} 
@@ -96,26 +50,17 @@ function FourOhFour() {
     onSortChange={handleSortChange}
     onFilterChange={handleFilterChange}
   />);
-} 
+}
+
+// https://www.oga.org.uk/boat_register/browse_the_register/boat.html?oga_no=315
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router basename={
-          // http is a check for dev mode, production will always be https
-          window.location.protocol==='http:'?'/':'/boats'
-        }>
-          <Switch>
-            <Route path="/boat/:id"><Boat /></Route>
-            <Route path="/about"><About /></Route>
-            <Route path="/designers"><Designers /></Route>
-            <Route path="/builders"><Builders /></Route>
-            <Route path="/fleets"><Fleets /></Route>
-            <Route path="/editors"><Editors /></Route>
-            <Route path="/support"><Support /></Route>
-            <Route path="/tech"><Tech /></Route>
-            <Route path="/"><FourOhFour /></Route>
-          </Switch>
+      <Router>
+        <Boat path='/browse_the_register/boat.html' />
+        <Register path='/boats_for_sale/boats_for_sale.html' sale={false}/>
+        <Register path='/browse_the_register/browse_the_register.html' sale={true}/>
       </Router>
     </ApolloProvider>
   );}
