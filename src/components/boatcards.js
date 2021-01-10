b83fd344d4055c1dd8797d7276a604a2eb8337ec
jcable <julian.cable@yahoo.com>
2021-01-10 19:49:55 +0000

    nice nothing found message

--- src/components/boatcards.js
+++ src/components/boatcards.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
-import toggle from '../images/toggle.gif';
 import { query, buildWhere } from '../util/cardquery';
 import { useBoatPagination } from '../util/BoatPagination';
 import BoatPagination from './boatpagination';
@@ -83,12 +83,69 @@ export default function BoatCards({
         </Container>
     );
   }
+
+  console.log(filters);
+
+  const { 
+    rig_type, construction_material, generic_type, 
+    design_class, year, sale, sail_type,
+    designer, builder, ogaNo,
+  } = filters;
+  let message;
+  if (ogaNo) {
+    message = `The boat numbered ${ogaNo} doesn't match the filters you have set`;
+  } else {
+    message = 'There are no';
+    if (rig_type) {
+      message = `${message} ${rig_type} rigged`;
+    }
+    if (design_class) {
+      message = `${message} ${design_class}'s`;
+    } else if (generic_type) {
+      message = `${message} ${generic_type}'s`;
+    } else {
+      message = `${message} boats`;
+    }
+    if (sail_type) {
+      message = `${message} with a ${sail_type} main`;
+    }
+    if (designer) {
+      message = `${message} by this designer`;
+    }
+    if (builder) {
+      message = `${message} by this builder`;
+    }
+    if (year) {
+      const { firstYear, lastYear } = year;
+      if (firstYear && lastYear) {
+        message = `${message} built between ${firstYear} and ${lastYear}`;
+      } else if (firstYear) {
+        message = `${message} built after ${firstYear}`;
+      } else if (lastYear) {
+        message = `${message} built before ${lastYear}`;
+      }
+    }
+    if (construction_material) {
+      message = `${message} built of ${construction_material}`;
+    }
+    if (sale) {
+      message = `${message} for sale.`;
+    }
+    message = `${message} on the register`;
+    if (filters['boat-name']) {
+      const name = filters['boat-name'];
+      message = `${message} which have ever been called ${name}`;
+    }
+  }
+
+
   return (
-    <div className="CenterItem">
-      <p>There are no boats which match the filter criteria you have set. Try
-      broadening the criteria. For example, if you have 'include boats without pictures' disabled, try enabling it.
-      </p>
-        <img src={toggle} alt="there is a toggle pictures control"/>
-    </div>
-  );
+    <Grid container alignItems='stretch'>
+    <Grid xs={2}></Grid>
+      <Grid>
+      <Typography variant='h6'>{message}.</Typography>
+      <Typography variant='h6'>Try removing some filters.</Typography>
+    </Grid>
+    <Grid xs={2}></Grid>
+  </Grid>);
 }
