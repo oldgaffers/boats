cd7e7fd06fab840592b211bacab7ad904d3605f9
jcable <julian.cable@yahoo.com>
2021-01-10 22:54:34 +0000

    seems to work

--- src/components/picker.js
+++ src/components/picker.js
@@ -1,66 +1,21 @@
 import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
-// import { validate } from 'graphql';
 
-export default function Picker({ options, id, label, onChange, value, clearable = true }) {
+export default function Picker({ options, id, label, onChange, value, clearable=true}) {
 
-  const [inputValue, setInputValue] = useState('');
+  const [value0, setValue0] = useState(value);
 
-  function handleChange(event, option, reason) {
-    let val;
-    switch (reason) {
-      case 'input':
-        // validate = '';
-        break;
-      case 'reset':
-        if (option) {
-          val = option;          
-        } else {
-          val = options[0].name;
-        }
-        break;
-      case 'clear':
-        val = '';
-        setInputValue('');
-        break;
-      case 'select-option':
-        val = option.name;
-        setInputValue(val);
-        break;
-      default:
-        console.log('unrecognised reason', reason);
-    }
-    onChange(id, val);
-  }
-
-  function v(option, val) {
-    //console.log(`${id} getOptionSelected ${option.name} !${val}!`);
-    if(!val) return false;
-    if(val === '""') console.log(`${id} getOptionSelected ${option.name} !${val}!`);
-    if(val.name) return option.name === val.name;
-    return option.name === val;
-  }
-  if (value && value !== inputValue) {
-    setInputValue(value);
-  }
-
-  return (
-    <Autocomplete
-      id={id}
-      options={options}
-      getOptionSelected={(option, val) => v(option,val)}
-      getOptionLabel={(option) => option.name?option.name:option}
-      value={value||''}
-      onChange={(event, option, reason) => handleChange(event, option, reason)}
-      inputValue={inputValue}
-      onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-      }}
-      renderInput={(params) => (
-        <TextField {...params} label={label} variant="outlined" />
-      )}
+  return (<Autocomplete
       disableClearable={!clearable}
-    />
-  );
-}
+      options={options.map(o => o.name)}
+      id={id}
+      value={value0}
+      onChange={(_, newValue) => {
+          setValue0(newValue);
+          onChange(id, newValue);
+      }}
+      renderInput={(params) => <TextField {...params} label={label} margin="normal" />}
+    />);
+};
+
