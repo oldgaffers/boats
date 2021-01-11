0dcf65d8670479cfaaec35aee56b7246dc40f740
jcable <julian.cable@yahoo.com>
2021-01-11 17:00:04 +0000

    make sure autocomplete is always controlled and make empty string an allowed option

--- src/components/picker.js
+++ src/components/picker.js
@@ -4,16 +4,19 @@ import { Autocomplete } from '@material-ui/lab';
 
 export default function Picker({ options, id, label, onChange, value, clearable=true}) {
 
-  const [value0, setValue0] = useState(value);
+  const [value0, setValue0] = useState(value || '');
+
+  const opt = options.map(o => o.name);
+  opt.push(''); // not set is an allowed value
 
   return (<Autocomplete
       disableClearable={!clearable}
-      options={options.map(o => o.name)}
+      options={opt}
       id={id}
       value={value0}
       onChange={(_, newValue) => {
           setValue0(newValue);
-          onChange(id, newValue);
+          onChange(id, newValue===''?undefined:newValue);
       }}
       renderInput={(params) => <TextField {...params} label={label} margin="normal" />}
     />);
