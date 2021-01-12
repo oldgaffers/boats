09240b6c763c8a32715ca5b6d969519fb15aa545
jcable <julian.cable@yahoo.com>
2021-01-12 02:35:13 +0000

    restore outline

--- src/components/picker.js
+++ src/components/picker.js
@@ -9,16 +9,16 @@ export default function Picker({ options, id, label, onChange, value, clearable=
   const opt = options.map(o => o.name);
   opt.push(''); // not set is an allowed value
 
-  return (<Autocomplete
-      disableClearable={!clearable}
-      options={opt}
-      id={id}
-      value={value0}
-      onChange={(_, newValue) => {
-          setValue0(newValue);
-          onChange(id, newValue===''?undefined:newValue);
-      }}
-      renderInput={(params) => <TextField {...params} label={label} margin="normal" />}
+  return (<Autocomplete      
+    disableClearable={!clearable}
+    options={opt}
+    id={id}
+    value={value0}
+    onChange={(_, newValue) => {
+        setValue0(newValue);
+        onChange(id, newValue===''?undefined:newValue);
+    }}
+    renderInput={(params) => <TextField {...params} label={label} margin="normal" variant="outlined"/>}
     />);
 };
 
