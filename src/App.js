7012375858bf610c2e7b2ecf5133a0e15cb44c99
jcable <julian.cable@yahoo.com>
2021-01-17 09:31:42 +0000

    oops

--- src/App.js
+++ src/App.js
@@ -1,126 +1,76 @@
 import React from 'react';
-import './App.css';
-import GqlBoatBrowser from './components/GqlBoatBrowser';
+import { CookiesProvider } from 'react-cookie';
+import { Link, Router, Route } from './components/mparouter';
 import OGAProvider from './util/gql';
+import GqlBoatBrowser from './components/GqlBoatBrowser';
 import Boat from './components/boat';
-import Designers from './components/designers';
-import Builders from './components/builders';
-import Fleets from './components/fleets';
-import About from './components/about';
-import Support from './components/support';
-import Tech from './components/tech';
-import Editors from './components/editors';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  useLocation,
-  useHistory,
-  Link,
-} from "react-router-dom";
+import BoatsForSaleIntro from './components/boatsforsaleintro';
+import BoatRegisterIntro from './components/boatregisterintro';
 
-const defaultState = {
-  page: 1,
+const browse = {
+  page: '1',
   bpp: '12', 
   sort: 'editors_choice', 
-  sortDirection: 'asc',
-  filters: { sale: false }, 
+  asc: 'true',
+  f_sale: 'false', 
 };
 
-function FourOhFour() {
+const buy = {
+  page: '1',
+  bpp: '12', 
+  sort: 'price', 
+  asc: 'false',
+  f_sale: 'true', 
+};
 
-  const location = useLocation();
-  const history = useHistory();
+const handlePageSizeChange = (bpp) => {
+  console.log('page size change', bpp);
+};
 
-  const { search, pathname, state } = location;
-
-  const sale = pathname === '/boat_register/boats_for_sale/boats_for_sale.html';
- 
-  if (!state) {
-    console.log('app state falsy, setting defaults');
-    history.replace('/', defaultState);
-  }
-
-  if (sale) {
-    if (state) {
-      state.filters.sale = true;
-      state.sort = 'price';
-      state.sortDirection = 'desc';
-    } else {
-      defaultState.filters.sale = true;
-      defaultState.sort = 'price';
-      defaultState.sortDirection = 'desc';  
-    }
-  }
-
-  const handlePageSizeChange = (bpp) => {
-    console.log('FourOhFour page size change', bpp);
-    history.replace('/', { ...state, bpp });
-  };
-
-  const handleSortChange = (field, dir) => {
-    console.log('FourOhFour sortchange', field, dir);
-    history.replace('/', { ...state, sort: field, sortDirection: dir });
-  }
-
-  const handleFilterChange = (filters) => {
-    console.log('FourOhFour filter change', filters);
-    history.replace('/', { ...state, filters });
-  }
-
-  const handlePageChange = (page) => {
-    console.log('FourOhFour page change', page);
-    history.replace('/', { ...state, page });
-  }
- 
-  if (search !== '') {
-    const params = new URLSearchParams(search);
-    const path = params.get('p');
-    console.log('FourOhFour path', path);
-    const oga_no = params.get('oga_no');
-    if (oga_no) {
-      return (<Redirect to={`/boat/${oga_no}`} />)
-    }
-    if(path) {
-      return (<Redirect to={path} />)
-    }
-  }
-  return (
-    <GqlBoatBrowser
-      title={sale?'Boats for Sale':'Browse the Register'}
-      defaultState={state || defaultState}
-      onPageSizeChange={handlePageSizeChange}
-      onSortChange={handleSortChange}
-      onFilterChange={handleFilterChange}
-      onPageChange={handlePageChange}
-      link={Link}
-      location={location}
-    />
-  );
-} 
-
-function App() {
-  return (
-    <OGAProvider>
-      <Router basename={
-          // http is a check for dev mode, production will always be https
-          window.location.protocol==='http:'?'/':'/boats'
-        }>
-          <Switch>
-            <Route path="/boat/:id"><Boat /></Route>
-            <Route path="/about"><About /></Route>
-            <Route path="/designers"><Designers /></Route>
-            <Route path="/builders"><Builders /></Route>
-            <Route path="/fleets"><Fleets /></Route>
-            <Route path="/editors"><Editors /></Route>
-            <Route path="/support"><Support /></Route>
-            <Route path="/tech"><Tech /></Route>
-            <Route path="/"><FourOhFour /></Route>
-          </Switch>
-      </Router>
-    </OGAProvider>
-  );
+const handleSortChange = (field, dir) => {
+  console.log('sortchange', field, dir);
 }
 
-export default App;
+const handleFilterChange = (filters) => {
+  console.log('filter change', filters);
+}
+
+const handlePageChange = (page) => {
+  console.log('page change', page);
+}
+
+export default function App() {
+  return (
+    <CookiesProvider>
+    <OGAProvider>
+    <Router>
+      <Route state={buy} path='/boats_for_sale/(?:test_)?boats_for_sale.*'>
+        <BoatsForSaleIntro/>
+        <GqlBoatBrowser
+          title='Boats for Sale'
+          onPageSizeChange={handlePageSizeChange}
+          onSortChange={handleSortChange}
+          onFilterChange={handleFilterChange}
+          onPageChange={handlePageChange}
+          link={Link}
+        />
+      </Route>
+      <Route state={browse} path='/browse_the_register/(?:test_)?browse_the_register.*'>
+        <BoatRegisterIntro/>
+        <GqlBoatBrowser
+          title='Browse the Register'
+          onPageSizeChange={handlePageSizeChange}
+          onSortChange={handleSortChange}
+          onFilterChange={handleFilterChange}
+          onPageChange={handlePageChange}
+          link={Link}
+        />
+      </Route>
+      <Route path='/browse_the_register/(?:test_)?boat.*' >
+        <Boat />
+      </Route>
+    </Router>
+    </OGAProvider>
+    </CookiesProvider>
+  );
+}
