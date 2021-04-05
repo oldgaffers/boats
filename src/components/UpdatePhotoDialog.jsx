09d1588f57c83f602d7cba587448ab997c7137a1
jcable <julian.cable@yahoo.com>
2021-04-05 08:22:35 +0100

    use dropzone for image uploads and delete all others

--- src/components/UpdatePhotoDialog.jsx
+++ src/components/UpdatePhotoDialog.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import Paper from '@material-ui/core/Paper';
-import FormWithFileUpload from './uploadphotos';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { DropzoneArea } from 'material-ui-dropzone'
 
 export const theme = createMuiTheme()
 
@@ -55,24 +59,66 @@ const useStyles = makeStyles((theme) => ({
 
 export default function UpdatePhotoDialog({ boat, onClose, open }) {
   const classes = useStyles();
+  const [pictures, setPictures] = useState([]);
+  const [email, setEmail] = useState(undefined);
+  const [copyright, setCopyright] = useState(undefined);
 
-  const handleCancel = () => {
-    onClose();
+  const onDrop = (p) => {
+    setPictures(p);
   }
 
-  const handleSave = (values, fileList) => { 
-    onClose(values, fileList);
-  };
+  const onUpload = () => {
+    onClose({ 
+      name: boat.name, albumKey: boat.image_key, oga_no: boat.oga_no,
+      copyright, email 
+    }, pictures);
+  }
+
+  const ready = () => {
+    if(pictures.length===0) return false;
+    if(!email) return false;
+    if(!copyright) return false;
+    return true;
+  }
+
+  const onEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const onCopyright = (e) => {
+    setCopyright(e.target.value);
+  }
 
   return (
-    <Dialog aria-labelledby="updateboat-dialog-title" open={open}>
-      <Paper className={classes.paper}>
-      <FormWithFileUpload classes={classes} onCancel={handleCancel} onSave={handleSave} boat={boat}/>
-      </Paper>
+  <Dialog aria-labelledby="updateboat-dialog-title" open={open}>
+      <Paper className={classes.paper}><Grid column spacing={3}>
+    <Grid item xs={12}>
+    <Typography variant="h5">Upload pictures for {boat.name}({boat.oga_no})</Typography>
+    </Grid>
+    <Grid item xs={12}>
+    <TextField required={true} type="email" label="Your Email" onChange={onEmail}/>
+    {/*<ImageForm onChange={onDrop}/>*/}
+    </Grid>
+    <Grid item xs={12}>
+    <DropzoneArea 
+      maxFileSize={5242880}
+      acceptedFiles={['image/*']}
+      onChange={onDrop}
+    />
+    </Grid>
+    <Grid item xs={12}>
+    <TextField required={true} type="text" label="Copyright Owner" onChange={onCopyright}/>
+    </Grid>
+    <Grid item xs={12}>
+    <div className={classes.buttons}>
+    <Button size="small" variant="contained" onClick={onClose}>Cancel</Button>
+    <Button disabled={!ready()} size="small" color="primary" variant="contained" onClick={onUpload}>Upload</Button>
+    </div>
+    </Grid>
+  </Grid></Paper>
     </Dialog>
   );
 }
-
 
 UpdatePhotoDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
