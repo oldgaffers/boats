10ce9d01dea27dbecae283411f15ed1da6529a5b
jcable <julian.cable@yahoo.com>
2021-04-05 13:56:52 +0100

    tidy

--- src/util/cardquery.jsx
+++ src/util/cardquery.jsx
@@ -34,6 +34,7 @@ export const query = gql`query boats(
 
 export const useCardQuery = (state) => {
   const { view, filters, page, bpp, sort, sortDirection } = state;
+  console.log(state);
   return useQuery(
     query, 
       {
