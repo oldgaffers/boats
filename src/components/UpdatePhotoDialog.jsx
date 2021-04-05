c63d7f30d7f5e6111397ff46a3fa17695096f789
jcable <julian.cable@yahoo.com>
2021-04-05 09:16:41 +0100

    layout

--- src/components/UpdatePhotoDialog.jsx
+++ src/components/UpdatePhotoDialog.jsx
@@ -1,55 +1,46 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import { DropzoneArea } from 'material-ui-dropzone'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { makeStyles, createMuiTheme } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { DropzoneArea } from "material-ui-dropzone";
 
-export const theme = createMuiTheme()
+export const theme = createMuiTheme();
 
 Object.assign(theme, {
   overrides: {
     root: {
-        width: "100%",
-        border: "1px",
-        marginBottom: 30,
-        paddingLeft: '50px',
-        paddingRight: '50px'
-    }
-  }
+      width: "100%",
+      border: "1px",
+      paddingLeft: "50px",
+      paddingRight: "50px",
+    },
+  },
 });
 
 const useStyles = makeStyles((theme) => ({
   layout: {
-    width: 'auto',
+    width: "auto",
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 600,
-      marginLeft: 'auto',
-      marginRight: 'auto',
+      marginLeft: "auto",
+      marginRight: "auto",
     },
   },
   paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: "25px 50px 75px 100px",// theme.spacing(2),
+    padding: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: "30px 30px 30px 30px",//theme.spacing(3),
+      padding: theme.spacing(3),
     },
   },
   stepper: {
     padding: theme.spacing(3, 0, 5),
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
   },
   button: {
     marginTop: theme.spacing(3),
@@ -65,57 +56,90 @@ export default function UpdatePhotoDialog({ boat, onClose, open }) {
 
   const onDrop = (p) => {
     setPictures(p);
-  }
+  };
 
   const onUpload = () => {
-    onClose({ 
-      name: boat.name, albumKey: boat.image_key, oga_no: boat.oga_no,
-      copyright, email 
-    }, pictures);
-  }
+    onClose(
+      {
+        name: boat.name,
+        albumKey: boat.image_key,
+        oga_no: boat.oga_no,
+        copyright,
+        email,
+      },
+      pictures
+    );
+  };
 
   const ready = () => {
-    if(pictures.length===0) return false;
-    if(!email) return false;
-    if(!copyright) return false;
+    if (pictures.length === 0) return false;
+    if (!email) return false;
+    if (!copyright) return false;
     return true;
-  }
+  };
 
   const onEmail = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   const onCopyright = (e) => {
     setCopyright(e.target.value);
-  }
+  };
 
   return (
-  <Dialog aria-labelledby="updateboat-dialog-title" open={open}>
-      <Paper className={classes.paper}><Grid column spacing={3}>
-    <Grid item xs={12}>
-    <Typography variant="h5">Upload pictures for {boat.name}({boat.oga_no})</Typography>
-    </Grid>
-    <Grid item xs={12}>
-    <TextField required={true} type="email" label="Your Email" onChange={onEmail}/>
-    {/*<ImageForm onChange={onDrop}/>*/}
-    </Grid>
-    <Grid item xs={12}>
-    <DropzoneArea 
-      maxFileSize={5242880}
-      acceptedFiles={['image/*']}
-      onChange={onDrop}
-    />
-    </Grid>
-    <Grid item xs={12}>
-    <TextField required={true} type="text" label="Copyright Owner" onChange={onCopyright}/>
-    </Grid>
-    <Grid item xs={12}>
-    <div className={classes.buttons}>
-    <Button size="small" variant="contained" onClick={onClose}>Cancel</Button>
-    <Button disabled={!ready()} size="small" color="primary" variant="contained" onClick={onUpload}>Upload</Button>
-    </div>
-    </Grid>
-  </Grid></Paper>
+    <Dialog aria-labelledby="updateboat-dialog-title" open={open}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography variant="h5">
+              Upload pictures for {boat.name} ({boat.oga_no})
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <DropzoneArea
+              maxFileSize={5242880}
+              acceptedFiles={["image/*"]}
+              onChange={onDrop}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required={true}
+              type="text"
+              label="Copyright Owner"
+              onChange={onCopyright}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required={true}
+              type="email"
+              label="Your Email"
+              onChange={onEmail}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container justify="space-between">
+              <Grid item>
+                <Button size="small" variant="contained" onClick={onClose}>
+                  Cancel
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  disabled={!ready()}
+                  size="small"
+                  color="primary"
+                  variant="contained"
+                  onClick={onUpload}
+                >
+                  Upload
+                </Button>                
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
     </Dialog>
   );
 }
