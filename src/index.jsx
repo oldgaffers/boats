5be55f343f39a7bcb4bb5b201de647965981849f
jcable <julian.cable@yahoo.com>
2021-06-07 08:22:40 +0100

    log

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+const L = () => {
+  console.log('location for oath redirect', window.location.href);
+  return '';
+};
+
 ReactDOM.render(
   <React.StrictMode>
+    <L/>
     <Auth0Provider
       domain="dev-uf87e942.eu.auth0.com"
       clientId="Mlm45jI7zvoQXbLSYSNV8F1qI1iTEnce"
