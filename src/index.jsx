6c392b2880946f05ce505840ad351c6e7a5f0bd0
jcable <julian.cable@yahoo.com>
2021-06-07 08:28:10 +0100

    query

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+const redirectUri = window.location.origin + window.location.pathname;
+
 const L = () => {
-  console.log('location for oath redirect', window.location.href);
+  console.log('location for oath redirect', redirectUri);
   return '';
 };
 
@@ -16,7 +18,7 @@ ReactDOM.render(
     <Auth0Provider
       domain="dev-uf87e942.eu.auth0.com"
       clientId="Mlm45jI7zvoQXbLSYSNV8F1qI1iTEnce"
-      redirectUri={window.location.href}
+      redirectUri={redirectUri}
     >
       <App />
     </Auth0Provider>
