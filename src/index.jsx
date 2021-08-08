6778039bd1e6111a85eae6cde430e76b5665fa36
jcable <julian.cable@yahoo.com>
2021-08-08 17:12:58 +0100

    tody

--- src/index.jsx
+++ src/index.jsx
@@ -5,20 +5,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-const redirectUri = window.location.origin + window.location.pathname;
-
-const L = () => {
-  console.log('location for oath redirect', redirectUri);
-  return '';
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <L/>
     <Auth0Provider
       domain="dev-uf87e942.eu.auth0.com"
       clientId="Mlm45jI7zvoQXbLSYSNV8F1qI1iTEnce"
-      redirectUri={redirectUri}
+      redirectUri={window.location.origin + window.location.pathname}
     >
       <App />
     </Auth0Provider>
