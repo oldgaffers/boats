7bc620461eb94b57728149a8d668205c96b4dfb5
jcable <julian.cable@yahoo.com>
2021-09-25 18:39:40 +0100

    use new form

--- src/components/smallboatsintro.jsx
+++ src/components/smallboatsintro.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import CreateBoatButton from './CreateBoat';
 
 export default function SmallBoatsIntro() {
     return (
@@ -13,15 +14,10 @@ export default function SmallBoatsIntro() {
           Filter the list using the options below, and then click the 'More' button
           to see all the pictures and information we have for that boat.
           </Typography>
-        <Typography variant="body1">
-            Have a boat and can't find it here? Fill in our{' '}
-            <a href="https://form.jotform.com/jfbcable/new-boat">form</a>
-             &nbsp;and we will add it.
-            </Typography>
-        <Typography variant="body1">
-            You can also use the form to suggest a boat whether you own it or
-            not.
-            </Typography>
+          <Typography variant="body1">
+            Know about a boat and can't find it here? Fill in our
+        </Typography>
+        <CreateBoatButton/>
         <Typography variant="body1">
             You can submit pictures, additions, and corrections to boats, or
             contact the owner from the boat's detail page.
