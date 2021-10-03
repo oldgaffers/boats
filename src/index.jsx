f5dc1759ca7b394622a01a5ca6484c45123b9bf6
jcable <julian.cable@yahoo.com>
2021-10-03 21:26:41 +0100

    logging

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,18 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+function url() {
+  const r = window.location.origin + window.location.pathname;
+  console.log('redirectUrl', r);
+  return r;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-uf87e942.eu.auth0.com"
       clientId="Mlm45jI7zvoQXbLSYSNV8F1qI1iTEnce"
-      redirectUri={window.location.origin + window.location.pathname}
+      redirectUri={url()}
     >
       <App />
     </Auth0Provider>
