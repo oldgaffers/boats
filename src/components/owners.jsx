ea97eaa439bf913471cc00d65ad82fd8e9c77165
jcable <julian.cable@yahoo.com>
2021-11-02 16:54:11 +0000

    wip

--- src/components/owners.jsx
+++ src/components/owners.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from '@material-ui/core/Typography';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
 export default function Owners({ classes, boat }) {
+    const owner = useQuery(gql(`query boat(where: {oga_no: {_eq: $${boat.oga_no}}}) {
+          name oga_no current_owners
+        }`));
+    const member = 5004
+    const members = useQuery(gql(`query members(member: ${member}) {
+          firstname lastname id
+        }
+      }`));
+    if (members.loading || owner.loading) return <CircularProgress />;
+    console.log(members, owner);
     return (<Typography>you will see owners here as you are a member</Typography>);
 }
