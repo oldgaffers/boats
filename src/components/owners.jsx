0c10c90bfe8674f2051d6191497a23b69fe3a07b
jcable <julian.cable@yahoo.com>
2021-11-02 18:55:19 +0000

    wip

--- src/components/owners.jsx
+++ src/components/owners.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
@@ -10,6 +17,7 @@ export default function Owners({ classes, boat }) {
           current_owners
         }
       }`));
+      /*
     const member = 5004
     const members = useQuery(gql(`query members {
         members(member: ${member}) {
@@ -17,8 +25,27 @@ export default function Owners({ classes, boat }) {
           lastname
           id
         }
-      }`));
-    if (members.loading || owner.loading) return <CircularProgress />;
+      }`)); */
+    if (owner.loading) return <CircularProgress />;
     console.log(members, owner);
-    return (<Typography>you will see owners here as you are a member</Typography>);
-}
+    return (
+        <TableContainer component={Paper}>
+        <Table className={classes.table} size="small" aria-label="owners">
+        <TableHead>
+            <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Share/leach</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {owner.data.boat[0].current_owners.map((row) => 
+            (<TableRow key={row.id}>
+                <TableCell align="right">{row.id}</TableCell>
+                <TableCell align="right">{row.share}</TableCell>
+            </TableRow>
+            ))}
+        </TableBody>
+        </Table>
+    </TableContainer>
+ 
