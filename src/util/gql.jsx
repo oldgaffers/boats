b3edd16318607883ace55a409c29f8b325d19dfc
jcable <julian.cable@yahoo.com>
2021-11-28 23:32:40 +0000

    auth

--- src/util/gql.jsx
+++ src/util/gql.jsx
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ApolloClient from "apollo-client"; // N.B. only needed for the enquiry mutation
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from '@apollo/react-hooks';
 import { useAuth0 } from "@auth0/auth0-react";
+import { setContext } from 'apollo-link-context'
+
+const httpLink = createHttpLink({
+  uri: "https://api-oga.herokuapp.com/v1/graphql",
+})
+
+const anonymousClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default function OGAProvider({children}) {
+    const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [accessToken, setAccessToken] = useState();
+    const [client, setClient] = useState(anonymousClient);
+
+    useEffect(() => {
+      const getAccessToken = async () => {
+      if (isAuthenticated) {
+          const token = await getAccessTokenSilently()
+          setAccessToken(token)
+        }
+      }
+      getAccessToken();
+    }, [isAuthenticated, getAccessTokenSilently])
+
+    useEffect(() => {
+      const authLink = setContext((_, { headers }) => {
+        if (accessToken) {
+          return {
+            headers: {
+              ...headers,
+              authorization: `Bearer ${accessToken}`,
+            },
+          }
+        } else {
+          return {
+            headers: {
+              ...headers,
+            },
+          }
+        }
+      })
   
-  export default function OGAProvider({children}) {
-    const { isAuthenticated, getTokenSilently } = useAuth0();
-
-    const headers = {};
-
-    if (isAuthenticated) {
-      headers.Authorization = `Bearer ${getTokenSilently()}`
-    }
-
-    const client = new ApolloClient({
-      link: createHttpLink({
-        uri: "https://api-oga.herokuapp.com/v1/graphql",
-        headers,
-      }),
-      cache: new InMemoryCache()
-    });
+      const client = new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+      })
+      setClient(client)
+    }, [accessToken]);
 
     return (<ApolloProvider client={client}>{children}</ApolloProvider>);
   }
