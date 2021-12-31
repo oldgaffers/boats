ade10da60d4a8363567361cdb9337655b73c209c
jcable <julian.cable@yahoo.com>
2021-12-31 11:26:19 +0000

    no background

--- src/components/smugmuggallery.jsx
+++ src/components/smugmuggallery.jsx
@@ -7,7 +7,6 @@ const queryString = 'autoStart=1&captions=1&navigation=1'
     +'&playButton=1&randomize=1&speed=3&transition=fade&transitionSpeed=2'
 
 const SlideShowIframe = styled(Iframe)(({ theme }) => ({
-    backgroundColor: theme.palette.primary.main,
     padding: theme.spacing(1),
     border: 'none !important',
     }));
