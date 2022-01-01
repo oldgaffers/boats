3c98711110482c3dbf03c0d0b998c8e2a5ab307b
jcable <julian.cable@yahoo.com>
2022-01-01 21:17:45 +0000

    no oga_no

--- src/components/boat.jsx
+++ src/components/boat.jsx
@@ -25,7 +25,7 @@ function getBoat(b) {
 }
 
 export default function Boat({location}) {
-  let oga_no = 1;
+  let oga_no = '';
   if (location) {
     const params = new URLSearchParams(location.search);
     oga_no = params.get('oga_no');  
@@ -46,20 +46,20 @@ export default function Boat({location}) {
 
   if (b.loading) return <p>Loading...</p>
   if (b.error) {
-      if (oga_no) {
-          return (<div>
-              Sorry, we had a problem getting the data for
-              the boat with OGA number {oga_no}
-              <p>Please try searching on the <a href={location.origin}>Main Page</a></p>
-              </div>);
-      } else {
-          return (<div>
-            <div>You've come to the boat detail viewer but we don't know what boat you wanted.</div>
-            <div>If you are looking for boats for sale please visit <a href={location.origin+'/boat_register/boats_for_sale/boats_for_sale.html'}>Boats for Sale</a></div>
-            <div>If you are looking interested in trailerable boats please visit <a href={location.origin+'/boat_register/small_boats/small_boats.html'}>Small Boats</a></div>
-            <div>To browse our full register please visit <a href={location.origin+'/boat_register/browse_the_register/browse_the_register.html'}>All Boats</a></div>
-              Otherwise please visit our <a href={location.origin}>Home Page</a>
-            </div>);
+      if (oga_no === '') {
+        return (<div>
+          <div>You've come to the boat detail viewer but we don't know what boat you wanted.</div>
+          <div>If you are looking for boats for sale please visit <a href={location.origin+'/boat_register/boats_for_sale/boats_for_sale.html'}>Boats for Sale</a></div>
+          <div>If you are looking interested in trailerable boats please visit <a href={location.origin+'/boat_register/small_boats/small_boats.html'}>Small Boats</a></div>
+          <div>To browse our full register please visit <a href={location.origin+'/boat_register/browse_the_register/browse_the_register.html'}>All Boats</a></div>
+            Otherwise please visit our <a href={location.origin}>Home Page</a>
+          </div>);
+    } else {
+        return (<div>
+          Sorry, we had a problem getting the data for
+          the boat with OGA number {oga_no}
+          <p>Please try searching on the <a href={location.origin}>Main Page</a></p>
+          </div>);
       }
   }
 
