07a6680cd73e3696d5cb5236e61a3f1d3cee8f4f
jcable <julian.cable@yahoo.com>
2022-01-06 07:01:48 +0000

    version

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 cp build/static/js/main.*.js.map .
 git add *.map
 cp build/static/js/main.*.js $1.js
-VERSION=`git tag |tail -1`
+VERSION=`npm run -s get_version`
+echo ${VERSION}
 sed -i -e "s/%%VERSION%%/${VERSION}/" $1.js
 git commit . -m ${VERSION}
 git push
