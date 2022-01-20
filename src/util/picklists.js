a28b0a9d9fbdcff4842652079d2a02c9d72f6513
jcable <julian.cable@yahoo.com>
2022-01-20 23:16:19 +0000

    make picklists right for view

--- src/util/picklists.js
+++ src/util/picklists.js
@@ -14,46 +14,51 @@ const basicQuery = gql(`{
     spar_material(order_by: {name: asc}){ name }
 }`);
 
-const parameterisedQuery = gql(`query lists(
-    $w1: designer_bool_exp!
-    $w2: builder_bool_exp!
-    $w3: rig_type_bool_exp!
-    $w4: design_class_bool_exp!
-    $w5: generic_type_bool_exp!
-    $w6: sail_type_bool_exp!
-    $w7: construction_material_bool_exp!
-    $w8: construction_method_bool_exp!
-    $w9: hull_form_bool_exp!
-    $wa: boat_bool_exp!
-)
-{
-    boat(where: $wa){name previous_names}
-    designer(order_by: {name: asc}, where: $w1){name id}
-    builder(order_by: {name: asc}, where: $w2){name id}
-    rig_type(order_by: {name: asc}, where: $w3){name}
-    design_class(order_by: {name: asc}, where: $w4){name id}
-    generic_type(order_by: {name: asc}, where: $w5){name}
-    sail_type(order_by: {name: asc}, where: $w6){name}
-    construction_material(order_by: {name: asc}, where: $w7){name}
-    construction_method(order_by: {name: asc}, where: $w8){name}
-    hull_form(order_by: {name: asc}, where: $w9){ name }
+const saleQuery = gql(`{
+    boat(where: {for_sale_state: {text: {_eq: "for_sale"}}}) { name previous_names }
+    designer(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { id name }
+    builder(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { id name }
+    design_class(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { id name }
+    rig_type(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+    generic_type(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+    sail_type(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+    construction_material(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+    construction_method(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+    hull_form(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+    spar_material(where: {boats: {for_sale_state: {text: {_eq: "for_sale"}}}}, order_by: {name: asc}) { name }
+}`);
+
+const smallQuery = gql(`{
+    boat(where: {generic_type: {_in: ["Dayboat","Dinghy"]}}) { name previous_names }
+    designer(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { id name }
+    design_class(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { id name }
+    builder(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { id name }
+    rig_type(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
+    generic_type(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
+    sail_type(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
+    construction_material(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
+    construction_method(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
+    hull_form(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
+    spar_material(order_by: {name: asc}, where: {boats: {generic_type: {_in: ["Dayboat", "Dinghy"]}}}) { name }
 }`);
 
 export function usePicklists(view={}) {
     // for now we only support boats for sale and small boats
-    let where = undefined;
-    let bwhere = undefined;
     let query = basicQuery;
-    let variables;
-    if (where) {
-        // can we have a generic _bool_exp?
-        query = parameterisedQuery;
-        variables = { variables: {
-            w1: where, w2: where, w3: where, w4: where, w5: where,
-            w6: where, w7: where, w8: where, w9: where, wa: bwhere,
-        } }
+    switch (view) {
+        case 'small':
+            // { "where":{"_and":[{"genericTypeByGenericType":{"name":{"_in":["Dinghy","Dayboat"]}}}]}
+            console.log('small boats view');
+            query = smallQuery;
+            break;
+        case 'sell':
+            console.log('boats for sale view');
+            query = saleQuery;
+            break;
+        default:
+            console.log('main register');
     }
-    return useQuery(query, variables);
+    return useQuery(query);
 }
 
 export function useLazyPicklists() {
