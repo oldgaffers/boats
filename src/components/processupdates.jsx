e5908c953c257221a002d6966cc28d9804e7c308
jcable <julian.cable@bbc.co.uk>
2022-02-06 23:11:20 +0000

    wip

--- src/components/processupdates.jsx
+++ src/components/processupdates.jsx
@@ -1,89 +1,110 @@
 import React from 'react';
-// import EditIcon from '@mui/icons-material/Edit';
-// import DeleteIcon from '@mui/icons-material/Delete';
-// import CheckIcon from '@mui/icons-material/Check';
-// import IconButton from '@mui/material/IconButton';
-import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { gql, useQuery } from '@apollo/client';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import DoneIcon from '@mui/icons-material/Done';
 import ChangeViewer from './changeviewer';
+import CircularProgress from "@mui/material/CircularProgress";
+import { DataGrid, GridToolbar, GridActionsCellItem } from '@mui/x-data-grid';
+import { useAuth0 } from "@auth0/auth0-react";
+import { gql, useLazyQuery } from '@apollo/client';
 
-/*
-const PENDING_COUNT_QUERY = gql`
-  query MyQuery {
-    boat_pending_updates_aggregate {
-      aggregate {
-        count
-      }
+const query = gql`query MyQuery {
+  boat_pending_updates {
+    boat
+    data
+    id
+    originator
+    uuid
+    boat_by_id {
+      name
+      oga_no
     }
   }
-`;
-*/
-const PENDING_QUERY = gql`
-  query MyQuery {
-    boat_pending_updates {
-      boat
-      data
-      originator
-      uuid
-      id
-    }
-  }
-`;
+}`;
 
 export default function ProcessUpdates() {
-    const { loading, error, data } = useQuery(PENDING_QUERY);
-    const handlePageChange = (page) => {
+  const { user, isAuthenticated } = useAuth0();
+  let roles = [];
+  if (isAuthenticated) {
+      roles = user['https://oga.org.uk/roles'] || [];
+  }
+  if(document.referrer.includes('localhost')) { roles.push('editor')}
+  const [getPending, pd] = useLazyQuery( query ); 
+  console.log(pd);
+  if (!pd.called) {
+    getPending();
+    return <CircularProgress />;
+  }
+  if (pd.loading) {
+    return <CircularProgress />;
+  }
+
+  const handlePageChange = (page) => {
         console.log('handlePageChange', page);
-    };
-    const handlePageSizeChange = (pageSize) => {
+  };
+
+  const handlePageSizeChange = (pageSize) => {
         console.log('handlePageSizeChange', pageSize);
-    }
+  }
 
-    const handleEditCellCommit = (params) => {
+  const handleEditCellCommit = (params) => {
         console.log('handleEditCellCommit', params);
-    };
+  };
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-
-    const columns = [
-        { field: 'oga_no', headerName: 'No', width: 90 },
-        { field: 'name', headerName: 'Name', width: 90 },
-        { field: 'originator', headerName: 'Contact', width: 200 },
-        { field: 'data', headerName: 'Change', width: 250, editable: true,
-            valueFormatter: (params) => {
-                return 'double click to see the change';
-            },
-            renderEditCell: (cellValues) => {
-                console.log(cellValues);
-
-                const handleChangeViewerClose = () => {
-                    console.log('handleChangeViewerClose');
-                    const { id, field } = cellValues;
-                    cellValues.api.commitCellChange({ id, field, value: 'boo'});
-                    cellValues.api.setCellMode(id, field, 'view');
-                };
-                
-                return (<ChangeViewer onClose={() => handleChangeViewerClose()} open={true} change={cellValues.row.data}/>);
-            }
+  const columns = [
+    { field: 'name', headerName: 'Boat Name', width: 90, valueGetter: (params) => params.row.boat_by_id.name },
+    { field: 'oga_no', headerName: 'OGA No.', width: 90, valueGetter: (params) => params.row.boat_by_id.oga_no },
+    { field: 'originator', headerName: 'Contact', width: 200 },
+    { field: 'data', headerName: 'Change', flex: 1, editable: true,
+        valueFormatter: (params) => {
+            return 'double click to see the change';
         },
-    ];
-    const pending = data.boat_pending_updates.map((item) => {
-        return {
-            ...item,
-            oga_no: item.data.new.oga_no, 
-            name: item.data.new.name,
-        };
-    });
+        renderEditCell: (cellValues) => {
+            console.log(cellValues);
 
-    return (
+            const handleChangeViewerClose = () => {
+                console.log('handleChangeViewerClose');
+                const { id, field } = cellValues;
+                cellValues.api.commitCellChange({ id, field, value: 'boo'});
+                cellValues.api.setCellMode(id, field, 'view');
+            };
+            
+            return (<ChangeViewer onClose={() => handleChangeViewerClose()} open={true} change={cellValues.row.data}/>);
+        }
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      getActions: (params) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={console.log(params)}
+            showInMenu
+          />,
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            onClick={console.log(params)}
+          />,
+          <GridActionsCellItem
+            icon={<DoneIcon />}
+            label="Commit"
+            onClick={console.log(params)}
+          />,      
+        ]
+    }
+  ];
+
+  return (
     <div style={{ height: 350, width: '100%' }}>
       <div style={{ display: 'flex', height: '100%' }}>
         <div style={{ flexGrow: 1 }}>
           <DataGrid
-            rows={pending} columns={columns} 
-            autoPageSize={true} 
+            rows={pd.data.boat_pending_updates}
+            columns={columns} 
             components={{ Toolbar: GridToolbar }}
+            autoHeight={true}
             onPageChange={(page, details) => handlePageChange(page, details)}
             onPageSizeChange={(pageSize) => handlePageSizeChange(pageSize)}
             onCellEditCommit={(params) => handleEditCellCommit(params)}
@@ -91,5 +112,5 @@ export default function ProcessUpdates() {
         </div>
       </div>
       </div>
-);
+  );
 }
--- src/components/processupdatesbutton.jsx
+++ src/components/processupdatesbutton.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import Button from "@mui/material/Button";
+import Badge from "@mui/material/Badge";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useAuth0 } from "@auth0/auth0-react";
+import { gql, useLazyQuery } from '@apollo/client';
+
+const query = gql`query MyQuery {
+  boat_pending_updates_aggregate {
+    aggregate {
+      count
+    }
+  }
+}`;
 
 export default function ProcessUpdatesButton() {
-
+  const { user, isAuthenticated } = useAuth0();
+  let roles = [];
+  if (isAuthenticated) {
+      roles = user['https://oga.org.uk/roles'] || [];
+  }
+  if(document.referrer.includes('localhost')) { roles.push('editor')}
+  const [getPending, pd] = useLazyQuery( query ); 
+  console.log(pd);
+  if (!pd.called) {
+    getPending();
+    return <CircularProgress />;
+  }
+  if (pd.loading) {
+    return <CircularProgress />;
+  }
+  const count = pd.data.boat_pending_updates_aggregate.aggregate.count;
   return (
-    <>
+    <Badge badgeContent={count>0 ? count : undefined} color="secondary">
       <Button
         size="small"
         variant="contained"
         color='primary'
         href="/boat_register/pending/"
       >Process Updates</Button>
-    </>
+    </Badge>
   );
 }
