bf779144ffcb8f3cfb4046bea4df6444fa181e65
jcable <julian.cable@bbc.co.uk>
2022-02-15 13:22:52 +0000

    wip

--- src/components/adminform.jsx
+++ src/components/adminform.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLazyQuery, gql } from '@apollo/client';
 import { FormRenderer, componentTypes, validatorTypes } from "@data-driven-forms/react-form-renderer";
 import {
   componentMapper,
@@ -35,7 +36,7 @@ export const schema = () => {
                     options: [
                       { label: 'feature', value: 'feature'},
                       { label: 'set gallery', value: 'gallery'},
-                      { label: 'set owner', value: 'own'},
+                      { label: 'set owner(s)', value: 'own'},
                       { label: 'set for-sale', value: 'sell'},
                       { label: 'set sold', value: 'sold'},
                     ],
@@ -75,10 +76,54 @@ export const schema = () => {
             nextStep: "done-step",
             fields: [
               {
-                component: componentTypes.PLAIN_TEXT,
-                name: "ddf.own",
-                label: "sorry, not yet",
-              }
+                name: "own",
+                component: componentTypes.SUB_FORM,
+                title: 'Set Owner(s)',
+                description: 'edit historical and current owners',
+                fields: [
+                  {
+                    component: componentTypes.FIELD_ARRAY,
+                    name: "boat.ownerships.owners",
+                    label: "Owners",
+                    fields: [
+                      { 
+                        name: 'name',
+                        label: 'Owner',
+                        component: componentTypes.TEXT_FIELD,
+                        resolveProps: (props, { meta, input }, formOptions) => {
+                          const { values } = formOptions.getState();
+                          const index = parseInt(input.name.split(/[[\]]/)[1]);
+                          const r = values.boat.ownerships.owners[index];
+                          if (r.name) {
+                            return { initialValue: r.name };
+                          }
+                          return { initialValue: `${r.member}/${r.id}` };
+                        }
+                      },
+                      { 
+                        name: 'start',
+                        label: 'From',
+                        component: componentTypes.TEXT_FIELD
+                      },
+                      { 
+                        name: 'end',
+                        label: 'To',
+                        component: componentTypes.TEXT_FIELD
+                      },
+                      { 
+                        name: 'share',
+                        label: 'Share',
+                        component: componentTypes.TEXT_FIELD,
+                      }
+                    ],
+                  },
+                  {
+                    component: componentTypes.TEXT_FIELD,
+                    name: "boat.name",
+                    label: "name",
+                  },
+                ],
+              },
           ],
           },          
           {
@@ -133,9 +178,38 @@ export const schema = () => {
   };
 };
 
-export default function AdminForm({ classes, onCancel, onSave, boat }) {
+const queryIf = (o) => o.member && (o.name === undefined || o.name.trim() === '');
+
+export default function AdminForm({ onCancel, onSave, boat }) {
+  const memberNumbers = boat.ownerships.owners.filter((o) => queryIf(o)).map((o) => o.member);
+  const [getMembers, getMembersResults] = useLazyQuery(gql(`query members($members: [Int]!) {
+    members(members: $members) {
+      firstname
+      lastname
+      member
+      id
+    }
+  }`));
+
+  if (memberNumbers.length > 0 && !getMembersResults.called) {
+    getMembers(memberNumbers);
+  }
+  if (getMembersResults.called && !getMembersResults.loading && !getMembersResults.error) {
+    boat.ownerships.owners = boat.ownerships.owners.map((owner) => {
+      if (owner.name) {
+        return owner;
+      }
+      const m = getMembersResults.data.members.find((r) => owner.member === r.member && owner.id === r.id);
+      return {
+        ...owner,
+        name: `${m[0].firstname} ${m[0].lastname}`
+      };
+    });
+  }
+
   const state = { 
     ddf: { activity: "feature" },
+    boat,
   };
 
   const handleSubmit = (values) => {
