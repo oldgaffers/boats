1a5c6c107f0452fa8abe98ff2ca293c85fc52015
jcable <julian.cable@bbc.co.uk>
2022-02-18 08:40:41 +0000

    revert

--- src/components/owners.jsx
+++ src/components/owners.jsx
@@ -1,58 +1,33 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import CircularProgress from "@mui/material/CircularProgress";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogActions from '@mui/material/DialogActions';
-import TextField from '@mui/material/TextField';
-import { DataGrid } from '@mui/x-data-grid';
-import { useMutation, useLazyQuery, gql } from '@apollo/client';
-import { v4 as uuidv4 } from 'uuid';
+import gql from 'graphql-tag';
+import { useLazyQuery } from '@apollo/client';
+
+function Owner({ owner }) {
+  const name = owner.name || owner.note
+  const share = owner.share ? `${owner.share}/64` : '';
+  return (
+    <TableRow key={owner.id}>
+        <TableCell align="left">{name}</TableCell>
+        <TableCell align="left">{owner.start || '?'}</TableCell>
+        <TableCell align="left">{owner.end || '-'}</TableCell>
+        <TableCell align="left">{share}</TableCell>
+    </TableRow>
+    );
+}
 
 const queryIf = (o) => o.member && (o.name === undefined || o.name.trim() === '');
 
-function ClaimDialog({ boat, onClose, open, start=2000 }) {
-  const yearRef = useRef('');
-  const shareRef = useRef('');
-
-  const handleCancel = () => {
-    onClose();
-  }
-
-  const handleSave = () => { 
-    onClose({ start: parseInt(yearRef.current.value, 10), share: parseInt(shareRef.current.value, 10) });
-  };
-
-  return (
-    <Dialog aria-labelledby="updateboat-dialog-title" open={open}>
-      <DialogTitle>I own {boat.name}</DialogTitle>
-      <DialogContent>
-          <DialogContentText>
-            That's great. Please tell us when you acquired {boat.name}.
-          </DialogContentText>
-          <DialogContentText>
-             If you share ownership with someone else enter the number of 64ths you own.
-          </DialogContentText>
-          <TextField label="Year Acquired" variant="standard"  inputProps={{ inputMode: 'numeric', pattern: '[0-9]{4}' }} defaultValue={start} inputRef={yearRef} />
-          <TextField label="Share" variant="standard"  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} defaultValue={64} inputRef={shareRef} />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Button onClick={handleSave}>Ok</Button>
-        </DialogActions>
-    </Dialog>
-  );
-}
-
-function OwnersTable({ owners, membership, email, boat }) {
-  const [claimOpen, setClaimOpen] = useState(false);
+function OwnersTable({ owners }) {
   const memberNumbers = owners.filter((o) => queryIf(o)).map((o) => o.member);
-  const [getMembers, getMembersResults] = useLazyQuery(gql(`query members {
+  const [getMembersResults, { loading, error, data }] = useLazyQuery(gql(`query members {
     members(members: ${JSON.stringify(memberNumbers)}) {
       firstname
       lastname
@@ -60,26 +35,22 @@ function OwnersTable({ owners, membership, email, boat }) {
       id
     }
   }`));
-  const [addOwner, addOwnerResults] = useMutation(gql`mutation addOwner($boat: uuid!, $current: String!, $field: String!, $originator: String!, $proposed: String!, $uuid: uuid!) {
-    insert_boat_pending_updates(objects: {boat: $boat, current: $current, field: $field, originator: $originator, proposed: $proposed, uuid: $uuid})
-    { affected_rows }
-  }`);
-  if (getMembersResults.loading) return <CircularProgress />;
+  if (loading) return <CircularProgress />;
   let ownersWithNames = owners;
-  if (getMembersResults.error) {
-    console.log(`Error! ${getMembersResults.error}`);
+  if (error) {
+    console.log(`Error! ${error}`);
   }
   let members = [];
-  if (getMembersResults.data) {
-    if (getMembersResults.data.members) {
-      members = getMembersResults.data.members;
+  if (data) {
+    if (data.members) {
+      members = data.members;
     }
   } else if (memberNumbers.length > 0) {
-    getMembers();
+    getMembersResults();
   }
-  ownersWithNames = owners.map((owner, index) => {
+  ownersWithNames = owners.map((owner) => {
     if (owner.name) {
-      return { ...owner, id: index };
+      return owner;
     }
     let name = '';
     const m = members.filter((member) => member.id === owner.id);
@@ -89,85 +60,45 @@ function OwnersTable({ owners, membership, email, boat }) {
     return {
       ...owner,
       name,
-      id: index,
-    }
-  }).sort((a, b) => a.start > b.start);
-  
-  const handleClaim = () => {
-    console.log('claim', membership);
-    setClaimOpen(true);
-  }
-  console.log('addOwnerResults', addOwnerResults); // TODO
-
-  const ends = owners.filter((o) => o.end).sort((a, b) => a.end < b.end);
-  const lastEnd =(ends.length>0) ? ends[0].end : undefined;
-
-  const handleClaimClose = (claim) => {
-    if (claim) {
-      console.log('handleClaimClose', claim, membership, boat.id);
-      addOwner({
-        variables: {
-        boat: boat.id,
-        field: 'ownerships', 
-        current: JSON.stringify({ owners }), 
-        proposed: JSON.stringify({ owners: [...owners, {current: true, ...membership, ...claim }]}),
-        originator: email, 
-        uuid: uuidv4()
-      }});
-    }
-    setClaimOpen(false);
-  }
-
-  const handleAddRow = () => console.log('add');
-
+    }  
+  });
   return (
-    <div style={{ width: '100%' }}>
-      <Box sx={{ height: 400, bgcolor: 'background.paper' }}>
-        <DataGrid
-          rows={ownersWithNames}
-          columns={[
-            { field: 'name', headerName: 'Name', flex: 1, valueGetter: ({ row }) => row.name || row.note },
-            { field: 'start', headerName: 'Start', width: 90, valueGetter: ({ row }) => row.start || '?' },
-            { field: 'end', headerName: 'End', width: 90, valueGetter: ({ row }) => row.end || '-' },
-            { field: 'share', headerName: 'Share', width: 90, valueGetter: ({ row }) => row.share ? `${row.share}/64` : '' },
-          ]}
-        />
-      </Box>
-      <Stack
-        sx={{ width: '100%', mb: 1 }}
-        direction="row"
-        alignItems="flex-start"
-        columnGap={1}
-      >
-        <Button size="small" onClick={handleClaim}>
-          This is my boat
-        </Button>
-        <ClaimDialog open={claimOpen} boat={boat} membership={membership} onClose={handleClaimClose} start={lastEnd} />
-        <Button size="small" onClick={handleAddRow}>
-          I know of other people who have owned her
-        </Button>
-      </Stack>
-    </div>
+    <Table size="small" aria-label="owners">
+    <TableHead>
+        <TableRow>
+        <TableCell align="left">Name</TableCell>
+        <TableCell align="left">From</TableCell>
+        <TableCell align="left">To</TableCell>
+        <TableCell align="left">Share</TableCell>
+        </TableRow>
+    </TableHead>
+    <TableBody>
+        {ownersWithNames
+          .sort((a, b) => a.start>b.start)
+          .map((owner, index) => (<Owner key={index} id={index} owner={owner}/>))
+        }
+    </TableBody>
+    </Table>
   );
 }
 
-export default function Owners({ boat, membership, email }) {
+export default function Owners({ boat }) {
   const { current, owners } = boat.ownerships || { current: [], owners: [] };
   if (owners && owners.length > 0) {
     return (
-      <Paper>
-        <OwnersTable owners={owners} membership={membership} boat={boat} email={email} />
-      </Paper>
-    );
+      <TableContainer component={Paper}>
+        <OwnersTable owners={owners}/>
+      </TableContainer>
+    );  
   }
   console.log('no owners, using current')
   if (current && current.length > 0) {
     return (
-      <Paper>
-        <OwnersTable owners={current} membership={membership} boat={boat} email={email} />
-      </Paper>
-    );
+      <TableContainer component={Paper}>
+        <OwnersTable owners={current}/>
+      </TableContainer>
+    );  
   }
   console.log('no current, returning empty div');
-  return (<div />);
+  return (<div/>);
 }
