5fe7bae1794082f4de096fdaa18fa6ad8fbac7c6
jcable <julian.cable@bbc.co.uk>
2022-02-20 21:07:42 +0000

    static

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ CURRENT=`git branch --show-current`
 git checkout gh-pages
 cp build/static/js/main.*.js.map .
 git add *.map
+git add static
 cp build/static/js/main.*.js $1.js
 git commit . -m ${npm_package_version}
 git push
