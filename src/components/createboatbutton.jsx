d2e29a917ac2b8def9a4cad9b2a3027613b5cef3
jcable <julian.cable@bbc.co.uk>
2022-03-27 09:06:47 +0100

    fix: pass more data

--- src/components/createboatbutton.jsx
+++ src/components/createboatbutton.jsx
@@ -7,7 +7,27 @@ import { boatf2m } from "../util/format";
 import CreateBoatDialog from "./createboatdialog";
 // const CreateBoatDialog = React.lazy(() => import("./createboatdialog"));
 
-export default function CreateBoatButton({ onSubmit=() => {}, onCancel=() => {} }) {
+function sendToPipedream(boatMetric, create, email, uuid, onSuccess, onError) {
+  formData.set("boat", JSON.stringify(boatMetric));
+  formData.set("create", JSON.stringify(create));
+  formData.set("copyright", copyright);
+  formData.set("email", email);
+  formData.set("uuid", uuid);
+  axios.post(
+    'https://ac861c76e041d1b288fba6a2f1d52bdb.m.pipedream.net',
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      params: {
+        pipedream_upload_body: 1,
+      },
+    },
+  ).then(response => onSuccess(response)).catch(error => onError(error));
+}
+
+export default function CreateBoatButton({ onSubmit = () => { }, onCancel = () => { } }) {
   const [open, setOpen] = useState(false);
   const [snackBarOpen, setSnackBarOpen] = useState(false);
 
@@ -18,76 +38,66 @@ export default function CreateBoatButton({ onSubmit=() => {}, onCancel=() => {} 
 
   const handleSend = (values) => {
     const { user, email, ddf, ...b } = values;
-    const { new_design_class, new_designer, new_builder, ...boat} = b;
+    const { new_design_class, new_designer, new_builder, ...boat } = b;
     const { fileList, copyright } = ddf;
     setOpen(false);
     const formData = new FormData();
-    if (fileList && fileList.length>0) {
-      for(let i=0; i<fileList.length; i++) {
+    if (fileList && fileList.length > 0) {
+      for (let i = 0; i < fileList.length; i++) {
         formData.set(`file[${i}]`, fileList[i]);
       }
     }
     const create = {};
-    if(new_design_class) {
+    if (new_design_class) {
       create.design_class = new_design_class;
     }
-    if(new_designer) {
+    if (new_designer) {
       create.designer = new_designer;
     }
-    if(new_builder) {
+    if (new_builder) {
       create.builder = new_builder;
     }
-    formData.set("boat", JSON.stringify(boatf2m(boat)));
-    formData.set("create", JSON.stringify(create));
-    formData.set("copyright", copyright);
-    formData.set("email", email);
-    formData.set("uuid", uuidv4());
-    axios.post(
-      'https://ac861c76e041d1b288fba6a2f1d52bdb.m.pipedream.net',
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        params: {
-          pipedream_upload_body: 1,
-        },
+    const boatMetric = boatf2m(boat);
+    const uuid = uuidv4();
+
+    onSubmit({ boat: boatMetric, create, email, uuid });
+
+    sendToPipedream(boatMetric, create, email, uuid, 
+      (response) => {
+        console.log(response);
+        setSnackBarOpen(true);
       },
-    ).then(response => {
-      console.log(response);
-      setSnackBarOpen(true);
-      onSubmit(boat);
-    }).catch(error => {
-      console.log('post', error);
-      // TODO snackbar from response.data
-    });      
-  };
+      (error) => {
+        console.log('post', error);
+        // TODO snackbar from response.data      
+      },
+    );
 
-  const snackBarClose = () => {
-    setSnackBarOpen(false);
-  };
+    const snackBarClose = () => {
+      setSnackBarOpen(false);
+    };
 
-  return (
-    <>
-      <Button
-        size="small"
-        variant="contained"
-        color='primary'
-        onClick={() => setOpen(true)}
-      >Add a Boat</Button>
+    return (
+      <>
+        <Button
+          size="small"
+          variant="contained"
+          color='primary'
+          onClick={() => setOpen(true)}
+        >Add a Boat</Button>
         <CreateBoatDialog
           open={open}
           onCancel={handleCancel}
           onSubmit={handleSend}
         />
-      <Snackbar
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        open={snackBarOpen}
-        autoHideDuration={2000}
-        onClose={snackBarClose}
-        message="Thanks, we'll get back to you."
-        severity="success"
-      />
-    </>
-  );
-}
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          open={snackBarOpen}
+          autoHideDuration={2000}
+          onClose={snackBarClose}
+          message="Thanks, we'll get back to you."
+          severity="success"
+        />
+      </>
+    );
+  }
