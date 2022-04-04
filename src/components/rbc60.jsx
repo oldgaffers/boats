d1eac347827c372718bc80bb174768e514dd38f5
jcable <julian.cable@bbc.co.uk>
2022-04-04 19:47:07 +0100

    fix: wip

--- src/components/rbc60.jsx
+++ src/components/rbc60.jsx
@@ -97,12 +97,27 @@ const port = (name, start, end) => {
     return name;
 }
 
-const legfield = (name, label) => {
+const viafield = (name, label) => {
+    return {
+        component: componentTypes.CHECKBOX,
+        name: `via.${name}`,
+        label: `I plan to sail the ${label} leg`,
+        dataType: dataTypes.BOOLEAN,
+        condition: {
+            when: 'crew',
+            is: false,
+        },
+
+    };
+};
+
+const crewlegfield = (name, label) => {
     return {
         component: componentTypes.TEXT_FIELD,
         name: `leg.${name}`,
-        helperText: 'leave blank or enter the maximum number of spaces you might have',
-        label: `crew spaces for the ${label} leg`,
+        helperText: 'If you might have space for OGA members to help crew,'
+            +' you can indicate the maximum number of spaces here.',
+        label: `possible spaces for the ${label} leg`,
         type: 'number',
         dataType: dataTypes.INTEGER,
         validate: [
@@ -112,6 +127,11 @@ const legfield = (name, label) => {
                 value: 0,
             }
         ],
+        condition: {
+            when: 'crew',
+            is: true,
+        },
+
     };
 };
 
@@ -124,10 +144,18 @@ export default function RBC60() {
     `);
     const { user, isAuthenticated } = useAuth0();
 
-    console.log('mutation result', result);
+    let userState;
+
+    console.log('user', user, 'isAuthenticated', isAuthenticated);
+
+    if (result) {
+        console.log('mutation', result);
+    }
 
     if (loading) return <CircularProgress />;
-    if (error) return <p>Error :(can't get picklists)</p>;
+    if (error) {
+        // return <p>Error :(can't get picklists)</p>;
+    }
     // let roles = [];
     let member = 'NOTMEMBER';
     if (isAuthenticated && user) {
@@ -135,8 +163,16 @@ export default function RBC60() {
         // if (user['https://oga.org.uk/roles']) {
         //    roles = user['https://oga.org.uk/roles'];
         //}
+        console.log('member', member);
+        if (member) {
+            userState = 'member';
+        } else {
+            userState = 'user';
+        }
+    } else {
+        userState = 'not logged in'
     }
-    let pickers = {};
+    let pickers = { boat: [] };
     if (data) {
         pickers = data;
     } else {
@@ -224,9 +260,10 @@ export default function RBC60() {
         ports.forEach(({ name, start, end, via }, index, list) => {
             if (index > 0) {
                 if (via) {
-                    fields.push(...via.map((leg) => legfield(leg, leg)));
+                    fields.push(...via.map((leg) => viafield(leg, leg)));
+                    fields.push(...via.map((leg) => crewlegfield(leg, leg)));
                 } else {
-                    fields.push(legfield(`${list[index - 1].name}_${name}`, `${list[index - 1].name} - ${name}`));
+                    fields.push(crewlegfield(`${list[index - 1].name}_${name}`, `${list[index - 1].name} - ${name}`));
                 }
             }
             fields.push({
@@ -238,6 +275,53 @@ export default function RBC60() {
         });
         return {
             fields: [
+                {
+                    component: componentTypes.TEXT_FIELD,
+                    label: 'Type of login',
+                    name: 'userState',
+                    isReadOnly: true,
+                    hideField: false,
+                    initialValue: userState,
+                },
+                {
+                    component: componentTypes.TEXT_FIELD,
+                    name: 'user',
+                    isReadOnly: true,
+                    hideField: true,
+                    label: 'USER',
+                    initialValue: user || 'NONE',
+                },
+                {
+                    component: componentTypes.CHECKBOX,
+                    name: 'ddf.login',
+                    label: "I have a login",
+                    helperText: "If you are currently a member and have a boat register login, please log in. We can use your login details to help you fill the form in.",
+                    variant: 'h6',
+                    sx: { marginTop: ".8em" },
+                    condition: {
+                        when: 'userState',
+                        is: 'not logged in',
+                    },
+                },
+                {
+                    component: componentTypes.CHECKBOX,
+                    name: 'ddf.nonmember',
+                    label: "If you are not currently a member and would like to join RBC60 you can do so by taking out a 12 month membership for 2023 https://oga.org.uk/about/membership.html",
+                    variant: 'h6',
+                    sx: { marginTop: ".8em" },
+                    condition: {
+                        or: [
+                            {
+                                when: 'userState',
+                                is: 'not logged in',                                
+                            },
+                            {
+                                when: 'userState',
+                                is: 'user',                                
+                            },
+                        ]
+                    },
+                },
                 {
                     component: componentTypes.PLAIN_TEXT,
                     name: 'ddf.about',
@@ -272,7 +356,7 @@ export default function RBC60() {
                 {
                     component: 'paypal',
                     name: 'payment',
-                    label: 'Reserve your flag',
+                    label: 'Sign Up and Reserve your flag',
                     helperText: 'We are asking all skippers to reserve a flag up front for £15.'
                         + ' This will help us know how many boats to plan for. '
                         + ' (This is not the real Paypal, log in with gmc@oga.org.uk as the username and oldgaffers as the password.'
@@ -331,10 +415,11 @@ export default function RBC60() {
                     sx: { marginTop: ".5em" }
                 },
                 {
-                    component: componentTypes.PLAIN_TEXT,
-                    name: 'ddf.ports2',
-                    label: 'If you can offer crewing opportunities for any of the legs please indicate how many spaces you might have',
-                    // variant: 'h6',
+                    component: componentTypes.CHECKBOX,
+                    name: 'crew',
+                    label: 'If you want to offer crewing opportunities for any of the legs, check here',
+                    helperText: 'You don\t need to decide now, just leave the box unchecked if you haven\'t decided yet',
+                    dataType: dataTypes.BOOLEAN,
                 },
                 ...fields,
                 {
@@ -378,7 +463,7 @@ export default function RBC60() {
                     name: 'ddf.val',
                     resolveProps: (props, { meta, input }, formOptions) => {
                         const countField = formOptions.getFieldState('ddf.count');
-                        console.log(countField);
+                        console.log('countField', countField);
                         const count = countField && countField.value;
                         console.log('count', count);
                         const boatField = formOptions.getFieldState('boat');
@@ -422,19 +507,40 @@ export default function RBC60() {
     return (
         <Paper>
             <Grid container spacing={2}>
-                <Grid xs={10}>
+                <Grid item xs={10}>
                     <Typography variant='h3'>OGA Round Britain Cruise 2023 - RBC60</Typography>
                 </Grid>
-                <Grid xs={2}>
+                <Grid item xs={2}>
                     <LoginButton />
                 </Grid>
-                <Grid xs={12}>
-                    <Typography variant='body2'>
-                        {user ? `Hello ${user.given_name || user.name}. ` : ''}The RBC starts at Ramsgate on 27 April 2023 and OGA members are welcome to join at any stage for part of the cruise or the whole circumnavigation. Festivities are being arranged at each of the Party Ports around the country, and all OGA members and their boats are welcome at these, not only those taking part in the RBC.  The ports and dates listed are the confirmed Party Ports.
-                        Please tick the boxes to indicate which ports you plan to visit. If you intend to complete the whole circumnavigation starting at Ramsgate then tick all.
+                <Grid item xs={12}>
+                    <Typography variant='body1'>
+                        {user ? `Hello ${user.given_name || user.name}. ` : ''}
+                        This form is for OGA members to sign up to sail all or part of the OGA 60 Round Britain Cruise (RBC60).
+                    </Typography>
+                    <Typography variant='body1'>
+                        If you are interested in joining all or part of the cruise on someone else's boat go to our <a href='https://oga.org.uk/oga60/rbc60_crewing_opportunities.html'>Crewing page</a>.
+                    </Typography>
+                    <Typography variant='body1'>
+                        If you are interested in bringing a boat one of the OGA60 small boat events go to our <a href='https://oga.org.uk/oga60/small_boat_events.html'>Small Boat Events page</a>.
+                    </Typography>
+                    <Typography variant='body1'>
+                        If you want more information about OGA60 in genreral go to the main <a href='https://oga.org.uk/oga60/oga60.html'>OGA60 page</a>.
+                    </Typography>
+                    <Typography variant='body1'>
+                        Festivities are being arranged at each of the Party Ports around the country, and all OGA members and their boats are welcome at these, not only those taking part in the RBC. 
+                    </Typography>
+                    <Typography variant='body1'>
+                        These events are organised by the Areas and you will be able to register for them separately from the main <a href='https://oga.org.uk/events/events.html'>Events page</a>.
+                    </Typography>
+                    <Typography variant='body1'>
+                        The ports and dates listed are the confirmed Party Ports.
+                    </Typography>
+                    <Typography variant='body1'>
+                        Please tick the boxes to indicate which ports you plan to visit. 
                     </Typography>
                 </Grid>
-                <Grid sx={12}>
+                <Grid item sx={12}>
                     <FormRenderer
                         schema={schema(ports)}
                         subscription={{ values: true }}
