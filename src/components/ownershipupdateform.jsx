878aaf4c8c63474eec3435d03c69d546d66958ca
jcable <julian.cable@bbc.co.uk>
2022-04-07 17:00:05 +0100

    fix: owner edit

--- src/components/ownershipupdateform.jsx
+++ src/components/ownershipupdateform.jsx
@@ -7,16 +7,8 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { DataGrid, GridActionsCellItem, GridEditInputCell } from '@mui/x-data-grid';
-import { useLazyQuery, gql } from '@apollo/client';
-import Switch from "@mui/material/Switch";
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import FormControlLabel from "@mui/material/FormControlLabel";
-import { Autocomplete } from '@mui/material';
+import { useQuery, useLazyQuery, gql } from '@apollo/client';
+import EditOwner from './editowner';
 
 const MEMBER_QUERY = gql(`query members($members: [Int]!) {
     members(members: $members) {
@@ -25,17 +17,6 @@ const MEMBER_QUERY = gql(`query members($members: [Int]!) {
       member
       id
       GDPR
-    }
-  }`);
-
-const MEMBER_NAME_QUERY = gql(`query members($name: String!) {
-    members(lastname: $name) {
-      firstname
-      lastname
-      member
-      id
-      GDPR
-      town area
     }
   }`);
 
@@ -62,164 +43,74 @@ export const ownershipUpdateForm = {
 
 const queryIf = (o) => o.member && (o.name === undefined || o.name.trim() === '');
 
-function MemberEditInputCell(props) {
-    const [getMembers, getMembersResults] = useLazyQuery(MEMBER_NAME_QUERY);
-    const [nameOrMember, setNameOrMember] = useState(props.value);
-    const [inputName, setInputName] = useState(props.value);
-    const [isMember, setIsMember] = useState(true);
-    const [isOpen, setIsOpen] = useState(true);
-
-    let members = [];
-    if (getMembersResults.data) {
-        if (getMembersResults.data.members) {
-            members = getMembersResults.data.members;
-        }        
-    }
-
-    const handleTextChange = (event) => {
-        const n = event.target.value;
-        setNameOrMember(n);
-        if (n.length > 1) {
-            getMembers({ variables: { name: n } });
-        }
-    };
-
-    const onIsMemberChange = (event) => {
-        console.log('onIsMemberChange', event.target.checked);
-        setIsMember(event.target.checked);
-    };
-
-    const handleSave = async (event) => {
-        const { id, api } = props;
-        if (isMember) {
-            console.log('updating member fields', id, nameOrMember.id, nameOrMember.member, api.setEditCellValue);
-            await api.setEditCellValue({ id, field: 'ID', value: nameOrMember.id }, event);
-            await api.setEditCellValue({ id, field: 'member', value: nameOrMember.member }, event);
-        } else {
-            await api.setEditCellValue({ id, field: 'name', value: nameOrMember }, event);
-        }
-        
-        setIsOpen(false);
-    };
-
-    return (
-        <div>
-            <Dialog open={isOpen} onClose={() => { setIsOpen(false); }}>
-                <DialogTitle>Owner Identity</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Ownership records are held on public storage so OGA Members are stored by their membership Id.
-                    </DialogContentText>
-                    <FormControlLabel
-                        onChange={onIsMemberChange}
-                        control={<Switch checked={isMember} />}
-                        label="OGA Member"
-                    />
-                    {isMember ?
-                        <Autocomplete
-                            options={members}
-                            autoComplete
-                            filterOptions={(x) => x}
-                            isOptionEqualToValue={(option, value) => option.id === value.id}
-                            loading={getMembersResults.loading}
-                            getOptionLabel={(m) => {
-                                return `${m.firstname} ${m.lastname} (${m.town}, ${m.area})`
-                            }}
-                            onChange={(event, newValue) => {
-                                setNameOrMember(newValue);
-                            }}
-                            inputValue={inputName}
-                            onInputChange={(event, val) => {
-                                setInputName(val);
-                            }}
-                            renderInput={(params) => (
-                                <TextField {...params}
-                                    variant='outlined'
-                                    label='Name'
-                                    fullWidth
-                                    value={nameOrMember}
-                                    onChange={handleTextChange}
-                                />
-                            )}
-                        />
-                        :
-                        <TextField
-                            variant='outlined'
-                            label='Name'
-                            fullWidth
-                            value={nameOrMember}
-                            onChange={handleTextChange}
-                        />
-                    }
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setIsOpen(false)}>Cancel</Button>
-                    <Button onClick={handleSave}>Update</Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
-}
-
 export default function OwnershipForm(props) {
     const { user } = useAuth0();
     const { input } = useFieldApi(props);
 
-    let membership;
-    if (user && user['https://oga.org.uk/id'] && user['https://oga.org.uk/member']) {
-        membership = {
-            id: parseInt(user['https://oga.org.uk/id']),
-            member: parseInt(user['https://oga.org.uk/member']),
-        };
-    }
-    const [getMembers, getMembersResults] = useLazyQuery(MEMBER_QUERY);
-    if (getMembersResults.loading) return <CircularProgress />;
+    const [owners, setOwners] = useState(() => {
+        const o = input.value.owners || [];
+        if (o.length === 0 && input.value.current) {
+            const current = input.value.current.map((c) => ({ ...c, current: true }));
+            return [...o, ...current];
+        }
+        return o;
+    });
 
-    const owners = input.value.owners || [];
-    if (owners.length === 0 && input.value.current) {
-        const current = input.value.current.map((c) => ({ ...c, current: true }));
-        owners.push(...current);
-    }
-
-    if (getMembersResults.error) {
-        console.log(`Error! ${getMembersResults.error}`);
-    }
-
-    const memberNumbers = [...new Set(owners.filter((o) => queryIf(o)).map((o) => o.member))];
-    if (membership && !memberNumbers.includes(membership.member)) {
-        memberNumbers.push(membership.member);
-        // be nice to use user.name but this isn't always provided.
-        // unless we add it to user metadata from GOLD
+    const membership = () => {
+        if (user && user['https://oga.org.uk/id'] && user['https://oga.org.uk/member']) {
+            return {
+               id: parseInt(user['https://oga.org.uk/id']),
+               member: parseInt(user['https://oga.org.uk/member']),
+           };
+       }
+       return undefined;
     }
 
     let theirBoat = false;
+
+    const memberNumbers = [...new Set(owners.filter((o) => queryIf(o)).map((o) => o.member))];
     if (membership) {
+        if(!memberNumbers.includes(membership.member)) {
+            memberNumbers.push(membership.member);
+        }
         const currentRecords = input.value.current || owners.filter((o) => o.current);
         const currentIds = currentRecords.map((o) => o.id);
         theirBoat = currentIds.includes(membership.id);
     }
 
-    let members = [];
+    const [getMembers, getMembersResults] = useLazyQuery(MEMBER_QUERY);
+
+    console.log('owners', owners);
+
+    if (getMembersResults.loading) return <CircularProgress />;
+
+    if (getMembersResults.error) {
+        console.log(`Error! ${getMembersResults.error}`);
+    }
+
+    const members = [];
     if (getMembersResults.data) {
         if (getMembersResults.data.members) {
-            members = getMembersResults.data.members;
+            members.push(...getMembersResults.data.members);
+        } else {
+            getMembers({ variables: {members: memberNumbers } });
         }
-    } else if (memberNumbers.length > 0) {
-        getMembers({ variables: { members: memberNumbers } });
     }
 
     const ownerNameSetter = ({ value, row }) => {
-        if (row.ID) {
+        console.log('ownerNameSetter', value, row);
+        if (row.goldId) {
             return row;
         }
         return { ...row, name: value };
     };
 
     const ownerNameGetter = (owner) => {
+        console.log('ownerNameGetter', owner);
         if (owner.name) {
             return owner.name;
         }
-        const m = members.filter((member) => member.id === owner.ID);
+        const m = members.filter((member) => member.id === owner.goldId);
         if (m.length > 0) {
             const { firstname, lastname, GDPR } = m[0];
             return { firstname, lastname, GDPR, ...owner };
@@ -231,10 +122,14 @@ export default function OwnershipForm(props) {
     }
 
     const ownerNameFormatter = (owner) => {
+        console.log('ownerNameFormatter', owner);
         if (typeof owner === 'string') {
             return owner;
         }
-        const m = members.filter((member) => member.id === owner.ID);
+        console.log('members', members);
+        console.log('goldId', owner.goldId);
+        const m = members.filter((member) => member.id === owner.goldId);
+        console.log('m', m);
         if (m.length > 0) {
             const { firstname, lastname, GDPR } = m[0];
             if (GDPR) {
@@ -250,11 +145,19 @@ export default function OwnershipForm(props) {
     }
 
     const ownerNameEditor = (params) => {
-        const { id, api, field, value, row } = params;
+        const { id, api, value, row } = params;
         if (typeof value === 'string' && value !== '') {
             return (<GridEditInputCell {...params} />);
         } else {
-            return (<MemberEditInputCell id={id} field={field} value={value} row={row} api={api} />);
+            const onSaveRow = async (ownership, event) => {
+                console.log('onSaveRow', ownership);
+                await api.setEditCellValue({ id, field: 'name', value: ownership.name }, event);
+                const o = [...owners];
+                o[id] = { ...owners[id], ...ownership };
+                console.log('setOwners', o);
+                setOwners(o);
+            }
+            return (<EditOwner value={row} onSave={onSaveRow} />);
         }
     }
 
@@ -262,17 +165,20 @@ export default function OwnershipForm(props) {
     const lastEnd = (ends.length > 0) ? ends[0].end : undefined;
 
     const handleClaim = () => {
+        console.log('handleClaim');
         const family = members.filter((m) => m.member === membership.member);
+        const no = [];
         family.forEach((m) => {
             const o = { start: lastEnd, id: m.id, member: m.member, current: true, share: Math.floor(64 / family.length) };
-            owners.push(o);
+            no.push(o);
         })
         // setOwnerships({ ...ownerships, owners });
+        setOwners([...owners, ...no]);
         input.onChange({ owners });
     }
 
     const handleAddRow = () => {
-        owners.push({ name: '', start: lastEnd, share: 64 });
+        setOwners([...owners, { name: '', start: lastEnd, share: 64 }]);
         // setOwnerships({ ...ownerships, owners });
         input.onChange({ owners });
     }
@@ -283,33 +189,20 @@ export default function OwnershipForm(props) {
         input.onChange({ owners: o });
     }
 
-    // set current = false if and end year is added
-    const handleCellEditCommit = (params) => {
-        const { field, value, id } = params;
-        console.log('P', params);
-        if (field === 'end' && value && value !== '') {
-            console.log('end updated');
-            console.log('entry is', owners[id])
-            if (owners[id].current) {
-                delete owners[id].current;
-            }
-        }
-        owners[id][field] = value;
-        console.log('O', owners);
-        input.onChange({ owners });
-    };
-
     const ownersWithId = owners.map((owner, index) => {
         return {
             ...owner,
             id: index,
-            ID: owner.id, // needed for ownerName
+            goldId: owner.id, // needed for ownerName
         }
     }).sort((a, b) => a.start > b.start);
+
+    console.log('ownersWithId', ownersWithId);
 
     return (
         <div style={{ height: 300, width: '100%' }}>
             <DataGrid
+                experimentalFeatures={{ newEditingApi: true }}
                 rows={ownersWithId}
                 columns={[
                     {
@@ -322,8 +215,8 @@ export default function OwnershipForm(props) {
                         valueSetter: ownerNameSetter,
                         renderEditCell: ownerNameEditor,
                     },
-                    { field: 'ID', headerName: 'ID', width: 0, editable: true, valueGetter: ({ row }) => row.ID || '?',  },
-                    { field: 'member', headerName: 'Member', width: 0, editable: true, valueGetter: ({ row }) => row.member || '?' },
+                    { field: 'goldId', headerName: 'goldId', width: 0, editable: true, hide: true,  },
+                    { field: 'member', headerName: 'Member', width: 0, editable: true, hide: true, },
                     { field: 'start', headerName: 'Start', width: 90, editable: true, valueGetter: ({ row }) => row.start || '?' },
                     { field: 'end', headerName: 'End', width: 90, editable: true, valueGetter: ({ row }) => row.end || '-' },
                     { field: 'share', headerName: 'Share', width: 90, editable: true, valueFormatter: ({ value }) => value ? `${value}/64` : '' },
@@ -341,12 +234,11 @@ export default function OwnershipForm(props) {
                     }
                 ]}
                 autoPageSize={true}
-                onCellEditCommit={handleCellEditCommit}
-                isCellEditable={(params) => !params.row.ID || params.field !== 'name'}
+                isCellEditable={(params) => true}
                 initialState={{
                     columns: {
                       columnVisibilityModel: {
-                        ID: false,
+                        goldId: false,
                         member: false,
                       },
                     },
