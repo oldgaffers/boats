422938f67c6767b8ffc6a041bc8565b8547fabc5
jcable <julian.cable@bbc.co.uk>
2022-04-29 13:37:53 +0100

    fix: add fleet view

--- src/components/rbc60entries.jsx
+++ src/components/rbc60entries.jsx
@@ -13,7 +13,68 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FleetIcon from "./fleeticon";
+import BoatCards from './boatcards';
 import LoginButton from './loginbutton';
+
+function FleetView() {
+  const onBoatMarked = () => console.log('marked');
+  const onBoatUnMarked = () => console.log('unmarked');
+  const onPageChange = () => console.log('pageChange');
+  const [getFleets, getFleetsResult] = useLazyQuery(gql`query fleet {
+    fleet(where: {name: {_eq: "RBC 60"}}) {
+      name
+      filters
+    }
+  }
+  `);
+
+  if (!getFleetsResult.called) {
+    getFleets();
+    return <CircularProgress />;
+  }
+
+  if (getFleetsResult.loading) {
+    return <CircularProgress />;
+  }
+
+  if (getFleetsResult.error) {
+    console.log(getFleetsResult.error);
+    return (<div>Sorry, something went wrong</div>);
+  }
+
+  console.log(getFleetsResult.data);
+
+  const fleets = getFleetsResult.data.fleet;
+
+  return (
+    <div>
+      {fleets.map((fleet) => {
+        const state = { filters: fleet.filters, bpp: 12, page: 1, sort: 'name', sortDirection: 'asc', view: 'app', };
+        return (<Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <FleetIcon/><Typography>&nbsp;&nbsp;{fleet.name}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <BoatCards
+            state={state} markList={state.filters.oga_nos} onChangePage={onPageChange}
+            onBoatMarked={onBoatMarked} onBoatUnMarked={onBoatUnMarked}
+          />
+        </AccordionDetails>
+      </Accordion>);
+      }
+    )}
+    </div>
+  );
+}
 
 const query = gql`query rbc60 { rbc60_notification { data id created_at } }`;
 
@@ -301,6 +362,9 @@ export default function RBC60Entryies() {
                     components={{ Toolbar: GridToolbar }}
                     autoHeight={true}
                 />
+            </div>
+            <div style={{ flexGrow: 1 }}>
+                <FleetView/>
             </div>
         </div>
     );
