ed83644b1b5ae2a27627174658ad86f355373e2d
jcable <julian.cable@bbc.co.uk>
2022-05-22 20:45:25 +0100

    refactor

--- src/components/oga60form.jsx
+++ src/components/oga60form.jsx
@@ -1,43 +1,37 @@
 import React, { useState } from 'react';
-import Dialog from "@mui/material/Dialog";
 import Grid from "@mui/material/Grid";
 import Snackbar from "@mui/material/Snackbar";
 import OGA60 from "./oga60interest";
 
-export default function OGA60Dialog({ onClose, open }) {
-  const [snackBarOpen, setSnackBarOpen] = useState(false);
+export default function OGA60Form() {
+    const [snackBarOpen, setSnackBarOpen] = useState(false);
 
-  const handleClose = () => {
-    setSnackBarOpen(true);
-  }
+    const handleClose = () => {
+        setSnackBarOpen(true);
+    }
 
-  const handleCancel = () => {
-      onClose();
-  }
+    const handleCancel = () => {
+        // eslint-disable-next-line no-restricted-globals
+        history.go(0);
+    }
 
-  const handleSnackBarClose = () => {
-    setSnackBarOpen(false);
-    onClose();
-  }
+    const handleSnackBarClose = () => {
+        setSnackBarOpen(false);
+        // eslint-disable-next-line no-restricted-globals
+        history.go(0);
+    }
 
-  return (
-    <>
-        <Dialog
-            open={open}
-            aria-labelledby="form-dialog-title"
-            maxWidth={'lg'}
-            onClose={onClose}
-        >
-        <Grid container spacing={1}>
-            <Grid item xs={12}>&nbsp;</Grid>
+    return (
+        <>
+            <Grid container spacing={1}>
+                <Grid item xs={12}>&nbsp;</Grid>
                 <Grid item xs={1}>&nbsp;</Grid>
                 <Grid item xs={10}><OGA60 onClose={handleClose} onCancel={handleCancel} /></Grid>
                 <Grid item xs={1}>&nbsp;</Grid>
                 <Grid item xs={12}>&nbsp;</Grid>
             </Grid>
-        </Dialog>
-        <Snackbar
-                sx={{backgroundColor: 'green'}}
+            <Snackbar
+                sx={{ backgroundColor: 'green' }}
                 anchorOrigin={{ vertical: "top", horizontal: "center" }}
                 open={snackBarOpen}
                 autoHideDuration={5000}
@@ -45,6 +39,6 @@ export default function OGA60Dialog({ onClose, open }) {
                 message="Thanks for your interest. You should receive an email to say we have your details."
                 severity="success"
             />
-    </>
-  );
+        </>
+    );
 }
