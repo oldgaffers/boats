0c1cdaaaa3b1c6a91aaa09e0e0e946e14bbe53e1
jcable <julian.cable@bbc.co.uk>
2022-10-07 07:46:52 +0100

    mock axios

--- src/components/boat.test.jsx
+++ src/components/boat.test.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
 import { MemoryRouter } from "react-router-dom";
 import { screen, render } from '@testing-library/react';
-import { MockedProvider } from "@apollo/react-testing";
-import gql from 'graphql-tag';
 import Boat from './boat';
-
-const mocks = [
-  {
-    request: {
-      query: gql`query{thumb(id:$id)}`,
-      variables: { id: 1 }
-    },
-    result: {
-      data: {
-        thumb: "https://thumbs.com/1.jpg"
-      }
-    }
-  },
-  {
-    request: {
-      query: gql`query{thumb(id:$id)}`,
-      variables: { id: 1}
-    },
-    error: new Error("Something went wrong")
-  }
-];
 
 test('renders learn react link', () => {
   render(
-    <MockedProvider mocks={mocks}>
       <MemoryRouter initialEntries={['/', '/?oga_no=1']} initialIndex={1} >
         <Boat/>
       </MemoryRouter>
-    </MockedProvider>
   );
   const wanted = screen.getByText(/Loading/);
   expect(wanted).toBeInTheDocument();
