d8c0c7db2c3cb1ce310bdb7c99ef1cf4fbccde94
jcable <julian.cable@bbc.co.uk>
2022-10-30 20:20:08 +0000

    fix: wip

--- src/components/adminbutton.jsx
+++ src/components/adminbutton.jsx
@@ -3,29 +3,13 @@ import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import Snackbar from '@mui/material/Snackbar';
 import AdminDialog from './admindialog';
-import { gql, useMutation } from '@apollo/client';
-
-const UPDATE_BOAT = gql`mutation updateBoat($id: uuid!, $change: boat_set_input) {
-  update_boat_by_pk(pk_columns: {id: $id}, _set: $change) {
-      id
-  }
-}`;
 
 export default function AdminButton({ classes, boat }) {
-  const [updateInProgress, setUpdateInProgress] = useState(false);
+  // const [updateInProgress, setUpdateInProgress] = useState(false);
   const [open, setOpen] = useState(false);
   const [snackBarOpen, setSnackBarOpen] = useState(false);
-  const [updateBoat, updateBoatResult] = useMutation(UPDATE_BOAT);
 
-  useEffect(() => {
-    const { data, loading, error, called } = updateBoatResult;
-    if ((!error) && (!loading) && called && updateInProgress) {
-      const u = data.update_boat_by_pk;
-      console.log('successfully updated a boat', u);
-      setSnackBarOpen(true);
-      setUpdateInProgress(false);
-    }
-  }, [updateBoatResult, updateInProgress]);
+
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -36,8 +20,8 @@ export default function AdminButton({ classes, boat }) {
     setOpen(false);
     if (changes) {
      console.log('admin post'); 
-     setUpdateInProgress(true);
-     updateBoat({ variables: { id: boat.id, change: { ownerships: changes.boat.ownerships } } });
+    //setUpdateInProgress(true);
+     //updateBoat();
     }
   };
 
