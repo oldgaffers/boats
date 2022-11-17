de1de70237ef0cccde75f8f2820f9429648da8a6
jcable <julian.cable@bbc.co.uk>
2022-11-17 18:47:39 +0000

    lint

--- src/components/editbutton.jsx
+++ src/components/editbutton.jsx
@@ -3,33 +3,7 @@ import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import Snackbar from '@mui/material/Snackbar';
 import UpdateBoatDialog from './updateboatdialog';
-import { CLEARED_VALUE } from './editboat';
 import { postBoatData } from './boatregisterposts';
-
-const changedKeys = (change) => {
-  const oldKeys = Object.keys(change.old);
-  return Object.keys(change.new).filter((key) => {
-    if (oldKeys.includes(key)) {
-        if (Array.isArray(change.old[key])) {
-            if (change.old[key].length !== change.new[key].length) {
-                return true;
-            }
-            if (change.old[key].length === 0) {
-                return false;
-            }
-            if (change.old[key].every((val, index) => val === change.new[key][index])) {
-              return false;
-            }
-            return true;
-        } else if (typeof change.old[key] === 'object') {
-          return JSON.stringify(change.old[key]) !== JSON.stringify(change.new[key]); // TODO nested
-        } else if (change.old[key] === change.new[key]) {
-            return false;
-        }
-    }
-    return true;
-  });
-}
 
 export default function EditButton({ classes, boat }) {
   const [open, setOpen] = useState(false);
