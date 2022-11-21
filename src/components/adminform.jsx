4ac13fe2cd02dcee32212677c5e0d2b8e446f8a2
jcable <julian.cable@bbc.co.uk>
2022-11-21 20:18:00 +0000

    fix: remove duplicate

--- src/components/adminform.jsx
+++ src/components/adminform.jsx
@@ -1,46 +1,22 @@
-import React, { useState, useEffect, useCallback } from "react";
-import _debounce from 'lodash/debounce';
-import { useLazyQuery, gql } from '@apollo/client';
+import React, { useState, useEffect } from "react";
 import FormRenderer from '@data-driven-forms/react-form-renderer/form-renderer';
 import componentTypes from '@data-driven-forms/react-form-renderer/component-types';
-import dataTypes from '@data-driven-forms/react-form-renderer/data-types';
 import FormTemplate from '@data-driven-forms/mui-component-mapper/form-template';
 import componentMapper from "@data-driven-forms/mui-component-mapper/component-mapper";
 import FormSpy from '@data-driven-forms/react-form-renderer/form-spy';
 import useFormApi from '@data-driven-forms/react-form-renderer/use-form-api';
 import TextField from '@data-driven-forms/mui-component-mapper/text-field';
-import Select from '@data-driven-forms/mui-component-mapper/select';
 import BoatIcon from "./boaticon";
 import BoatAnchoredIcon from "./boatanchoredicon";
-import { HtmlEditor } from "./ddf/RTE";
-// const BoatIcon = React.lazy(() => import("./boaticon"));
-// const BoatAnchoredIcon = React.lazy(() => import("./boatanchoredicon"));
-// const { HtmlEditor } = React.lazy(() => import("./ddf/RTE"));
-
-const MEMBER_QUERY = gql(`query members($members: [Int]!) {
-  members(members: $members) {
-    firstname
-    lastname
-    member
-    id
-  }
-}`);
 
 const FieldListener = (props) => {
-  console.log('PROPS', props);
-  const [getMembers, getMembersResults] = useLazyQuery(MEMBER_QUERY);
   const [field, setField] = useState();
   const { getFieldState, getState, change } = useFormApi();
   const modified = getState().modified;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const dbGetMembers = useCallback(_debounce(getMembers, 1000), []);
-  
-  if (field && modified[field]) {
-    const member = getFieldState(field).value;
-    console.log('V', member);
-    if (!getMembersResults.called) {
-      dbGetMembers({ variables: { members: [member] }});
-    }
+
+  console.log(getFieldState(field).value);
+  if (field === 'some non-existing field') {
+    change(field.replace('member', 'id'), 'something');
   }
 
   useEffect(() => {
@@ -51,37 +27,6 @@ const FieldListener = (props) => {
     }
   }, [modified]);
 
-  if (getMembersResults.called) {
-    if (getMembersResults.loading) {
-      console.log('still loading');
-    } else {
-      if (getMembersResults.error) {
-        console.log(getMembersResults.error);
-      } else {
-        const m = getMembersResults.data.members;
-        if (m.length > 0) {
-          console.log(m);
-          const member = getFieldState(field).value;
-          if (m[0].member === member) {
-            console.log('got a good answer');
-            if(m.length === 1) {
-              change(field.replace('member', 'id'), m[0].id);
-              change(field.replace('member', 'share'), 64);
-              change(field.replace('member', 'current'), true);
-              return (<TextField {...props} value={`${m[0].firstname} ${m[0].lastname}`}/>);
-            }
-            return (<Select {...props} options={m.map((o) => ({
-              label: `${o.firstname} ${o.lastname}`,
-              value: o,
-            }))}/>);
-          } else {
-            console.log('user field has changed - refetch');
-            getMembers({ variables: { members: [member] }});
-          }
-        }
-      }
-    }
-  }
   return (<TextField {...props}/>);
 };
 
@@ -111,9 +56,6 @@ export const schema = () => {
                     options: [
                       { label: 'feature', value: 'feature'},
                       { label: 'set gallery', value: 'gallery'},
-                      { label: 'set owner(s)', value: 'own'},
-                      { label: 'set for-sale', value: 'sell'},
-                      { label: 'set sold', value: 'sold'},
                     ],
                     RadioProps: {
                       icon: <BoatAnchoredIcon color="primary" />,
@@ -145,102 +87,6 @@ export const schema = () => {
                 label: "sorry, not yet",
               }
           ],
-          },          
-          {
-            name: "own-step",
-            nextStep: "done-step",
-            fields: [
-              {
-                name: "own",
-                component: componentTypes.SUB_FORM,
-                title: 'Set Owner(s)',
-                description: 'edit historical and current owners',
-                fields: [
-                  {
-                    component: componentTypes.FIELD_ARRAY,
-                    name: "boat.ownerships",
-                    label: "Owners",
-                    fields: [
-                      { 
-                        name: 'member',
-                        label: 'Membership Number',
-                        component: componentTypes.TEXT_FIELD,
-                        type: 'string',
-                        dataType: dataTypes.INTEGER,
-                      },
-                      { 
-                        name: 'id',
-                        label: 'GOLD ID',
-                        component: componentTypes.TEXT_FIELD,
-                        type: 'string',
-                        dataType: dataTypes.INTEGER,
-                        /*
-                        resolveProps: (props, { meta, input }, formOptions) => {
-                          const state = formOptions.getState();
-                          if (state.dirty) {
-                            onChooseDesignClass(input.value);
-                          }
-                        },*/
-                      },
-                      { 
-                        name: 'name',
-                        label: 'Owner',
-                        component: 'field-listener',
-                      },
-                      { 
-                        name: 'start',
-                        label: 'From',
-                        component: componentTypes.TEXT_FIELD,
-                        type: 'string',
-                        dataType: dataTypes.INTEGER,
-                      },
-                      { 
-                        name: 'end',
-                        label: 'To',
-                        component: componentTypes.TEXT_FIELD,
-                        type: 'string',
-                        dataType: dataTypes.INTEGER,
-                      },
-                      { 
-                        name: 'current',
-                        label: 'current',
-                        component: componentTypes.CHECKBOX,
-                      },
-                      { 
-                        name: 'share',
-                        label: 'Share',
-                        component: componentTypes.TEXT_FIELD,
-                        type: 'string',
-                        dataType: dataTypes.INTEGER,
-                        default: 64,
-                      },
-                    ],
-                  },
-                ],
-              },
-          ],
-          },          
-          {
-            name: "sell-step",
-            nextStep: "done-step",
-            fields: [
-              {
-                component: componentTypes.PLAIN_TEXT,
-                name: "ddf.sell",
-                label: "sorry, not yet",
-              }
-          ],
-          },
-          {
-            name: "sold-step",
-            nextStep: "done-step",
-            fields: [
-              {
-                component: componentTypes.PLAIN_TEXT,
-                name: "ddf.sold",
-                label: "sorry, not yet",
-              }
-            ],
           },
           {
             name: "done-step",
@@ -259,47 +105,7 @@ export const schema = () => {
   };
 };
 
-const queryIf = (o) => o.member && (o.name === undefined || o.name.trim() === '');
-
 export default function AdminForm({ onCancel, onSave, boat }) {
-  const [getMembers, getMembersResults] = useLazyQuery(MEMBER_QUERY);
-  if (boat.ownerships) {
-    const current = boat.ownerships?.filter((o) => o.current) || [];
-    const memberNumbers = [...new Set(boat.ownerships.filter((o) => queryIf(o)).map((o) => o.member))];
-    if (memberNumbers.length > 0 && !getMembersResults.called) {
-      getMembers({ variables: { members: memberNumbers }});
-    }
-    if (getMembersResults.called) {
-      if (getMembersResults.loading) {
-        console.log('still loading');
-      } else {
-        if (getMembersResults.error) {
-          console.log(getMembersResults.error);
-        } else {
-          boat.ownerships = boat.ownerships.map((owner) => {
-            if (owner.name) {
-              return owner;
-            }
-            const m = getMembersResults.data.members.find((r) => owner.member === r.member && owner.id === r.id);
-            return {
-              ...owner,
-              name: `${m.firstname} ${m.lastname} (${m.member}:${m.id})`
-            };
-          });    
-        }
-      }
-    }
-
-    boat.ownerships = boat.ownerships.map((owner) => {
-      if (owner.member && current) {
-        const c = current.find((r) => owner.member === r.member && owner.id === r.id);
-        if (c) {
-          return { ...owner, current: true }
-        }
-      }
-      return owner;
-    });
-  }
 
   const state = { 
     ddf: { activity: "feature" },
@@ -307,13 +113,6 @@ export default function AdminForm({ onCancel, onSave, boat }) {
   };
 
   const handleSubmit = (values) => {
-    if (values.boat.ownerships) {
-      values.boat.ownerships.forEach((owner) => {
-        if (owner.member) {
-          delete owner.name;
-        }
-      });
-    }
     onSave({ boat: values.boat });
   };
 
@@ -322,7 +121,6 @@ export default function AdminForm({ onCancel, onSave, boat }) {
         schema={schema()}
         componentMapper={{
           ...componentMapper,
-          html: HtmlEditor,
           'field-listener': FieldListenerWrapper,
         }}
         FormTemplate={(props) => (
