c62416815c5416df2fe61c4d6b7af0f44f8e1c1e
jcable <julian.cable@bbc.co.uk>
2022-12-01 04:49:32 +0000

    refactor

--- src/components/fleetview.jsx
+++ src/components/fleetview.jsx
@@ -12,51 +12,52 @@ import { TokenContext } from './TokenProvider';
 import { getScopedData } from './boatregisterposts';
 
 export default function FleetView({ filters }) {
-    const [page, setPage] = useState(1);
-    const [data, setData] = useState();
-    const accessToken = useContext(TokenContext);
-  
-    const onBoatMarked = () => console.log('marked');
-    const onBoatUnMarked = () => console.log('unmarked');
-    const onPageChange = (n) => setPage(n.page);
+  const [data, setData] = useState();
+  const accessToken = useContext(TokenContext);
 
-    const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
 
-    useEffect(() => {
-      const getData = async () => {
-        const p = getScopedData('member', 'fleets', filters, accessToken);
-        setData(p.data);
-      }
-      if (accessToken && isAuthenticated) {
-        getData();
-      }
-    }, [accessToken, user, isAuthenticated, filters])
-  
-    if (!isAuthenticated) {
-      return (<div>Please log in to view this page</div>);
+  useEffect(() => {
+    const getData = async () => {
+      const p = await getScopedData('member', 'fleets', filters, accessToken);
+      setData(p.data);
     }
-  
-    if (!data) {
-        return <CircularProgress/>;
+    if (accessToken && isAuthenticated) {
+      getData();
     }
+  }, [accessToken, user, isAuthenticated, filters])
 
-    const fleet = data.Items[0]; 
-  
-    const state = { filters: fleet.filters, bpp: 12, page, sort: 'name', sortDirection: 'asc', view: 'app', };
+  if (!isAuthenticated) {
+    return (<div>Please log in to view this page</div>);
+  }
 
-    return (<Accordion>
-        <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-        >
-            <FleetIcon /><Typography>&nbsp;&nbsp;{fleet.name}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-            <BoatCards
-                state={state} markList={state.filters.oga_nos} onChangePage={onPageChange}
-                onBoatMarked={onBoatMarked} onBoatUnMarked={onBoatUnMarked}
-            />
-        </AccordionDetails>
-    </Accordion>);
+  if (!data) {
+    return <CircularProgress />;
+  }
+
+  return <FleetDisplay fleet={data.Items[0]} />;
+}
+
+export function FleetDisplay({ fleet }) {
+  console.log('FleetDisplay', fleet);
+  const [page, setPage] = useState(1);
+
+  const onPageChange = (n) => setPage(n.page);
+
+  const state = { filters: fleet.filters, bpp: 12, page, sort: 'name', sortDirection: 'asc', view: 'app', };
+
+  return (<Accordion>
+    <AccordionSummary
+      expandIcon={<ExpandMoreIcon />}
+      aria-controls="panel1a-content"
+      id="panel1a-header"
+    >
+      <FleetIcon /><Typography>&nbsp;&nbsp;{fleet.name}</Typography>
+    </AccordionSummary>
+    <AccordionDetails>
+      <BoatCards
+        state={state} onChangePage={onPageChange}
+      />
+    </AccordionDetails>
+  </Accordion>);
 }
