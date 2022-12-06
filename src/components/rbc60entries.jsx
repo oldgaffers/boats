aec132c46ca724f80f83d3325dc569815d391d2d
jcable <julian.cable@bbc.co.uk>
2022-12-06 23:27:55 +0000

    feat: email skippers

--- src/components/rbc60entries.jsx
+++ src/components/rbc60entries.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useState, memo, useRef, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import SendIcon from "@mui/icons-material/Send";
 import CircularProgress from "@mui/material/CircularProgress";
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import Grid from "@mui/material/Grid";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Popper from '@mui/material/Popper';
 import Table from '@mui/material/Table';
@@ -14,7 +16,9 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { TokenContext } from './TokenProvider';
 import { FleetDisplay } from './fleetview';
-import { getScopedData } from './boatregisterposts';
+import { getScopedData, postGeneralEnquiry } from './boatregisterposts';
+import LoginButton from './loginbutton';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Popover, TextField } from '@mui/material';
 
 function isOverflown(element) {
     return (
@@ -90,7 +94,7 @@ const GridCellExpandStrings = memo(function GridCellExpand(props) {
                 ref={cellValue}
                 sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
             >
-                {value?.join(', ')||''}
+                {value?.join(', ') || ''}
             </Box>
             {showPopper && (
                 <Popper
@@ -244,55 +248,153 @@ function renderCellExpandObjects(params) {
     );
 }
 
-function EntryTable({ rows }) {
-    const columns = [
-        { field: 'boat', headerName: 'Boat Name', width: 120, valueGetter: (params) => params.row.data.boat?.name||'' },
-        { field: 'oga_no', headerName: 'OGA No.', width: 80, valueGetter: (params) => params.row.data.boat?.oga_no||'' },
-        // { field: 'skipper', headerName: 'Skipper', width: 100, valueGetter: (params) => params.row.data.user.name },
-        // { field: 'created_at', headerName: 'Submitted', width: 100, valueFormatter: (params) => new Date(params.value).toLocaleDateString() },
-        { field: 'data.rbc', headerName: 'Circumnavigating', width: 100, valueGetter: (params) => params.row.data.rbc || false, valueFormatter: (params) => params.value ? 'Yes' : 'No' },
-        {
-            field: 'port',
-            headerName: 'Ports',
-            width: 400,
-            valueGetter: (params) => params.row.data.port || [],
-            renderCell: renderCellExpandStrings,
-        },
-        {
-            field: 'leg',
-            headerName: 'Crewing Spaces offered per leg',
-            width: 300,
-            valueGetter: (params) => params.row.data.leg || [],
-            renderCell: renderCellExpandObjects,
-        },
-        { field: 'data.ecc', headerName: 'EC Cruise', width: 100, valueGetter: (params) => params.row.data.ecc, valueFormatter: (params) => params.value ? 'Yes' : '' },
-    ];
-
-    return (<div style={{ display: 'flex', height: '100%' }}>
-        <div style={{ flexGrow: 1 }}>
-            <DataGrid
-                rows={rows.map((row, index) => ({ ...row, id: index }))}
-                columns={columns}
-                components={{ Toolbar: GridToolbar }}
-                autoHeight={true}
-            />
-        </div>
-    </div>);
-}
-
 export default function RBC60Entryies() {
     const name = 'RBC 60';
     const accessToken = useContext(TokenContext);
     const { user, isAuthenticated } = useAuth0();
     const [data, setData] = useState();
 
+    function ContactDialog({ boat, open, onSend, onCancel }) {
+        const [text, setText] = useState("");
+
+        console.log(boat);
+
+        const onClick = () => {
+            onSend(text);
+        }
+
+        return (
+            <Dialog
+                open={open}
+                onClose={onCancel}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Email the Skipper{boat?.name ? ` of ${boat.name}` : ''}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText variant="subtitle2">
+                        We will send your message to the skipper and give them your email address
+                    </DialogContentText>
+                    <TextField
+                        onChange={(e) => setText(e.target.value)}
+                        margin="dense"
+                        label="About your enquiry"
+                        type="text"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onCancel} color="primary">
+                        Cancel
+                    </Button>
+                    <Button
+                        endIcon={<SendIcon />}
+                        onClick={onClick}
+                        color="primary"
+                    >
+                        Send
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+
+    function ContactButton({ params }) {
+        const [open, setOpen] = useState(false);
+        const [popoverOpen, setPopoverOpen] = useState(false);
+        const [anchorEl, setAnchorEl] = useState();
+        const buttonRef = useRef();
+
+        const { data, email } = params.row;
+
+        const onSend = (text) => {
+            setOpen(false);
+            setAnchorEl(buttonRef.current);
+            const data = {
+                topic: 'RBC 60', to: [email], message: text,
+                cc: [user.email], subject: `enquiry from ${user.name}`,
+            };
+            setPopoverOpen(true);
+            postGeneralEnquiry('member', 'entries', data)
+                .then((response) => {
+                    setPopoverOpen(false);
+                })
+                .catch((error) => {
+                    console.log("post", error);
+                    setPopoverOpen(false);
+                });
+        };
+
+        const onCancel = () => {
+            setOpen(false);
+        };
+
+        const handleClickOpen = () => {
+            setOpen(true);
+        };
+
+        return <>
+            <Button ref={buttonRef}
+                size='small' variant='outlined' onClick={handleClickOpen}
+            >Press Me</Button>
+            <ContactDialog boat={data.boat} open={open} onSend={onSend} onCancel={onCancel} />
+            <Popover open={popoverOpen}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(undefined)}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}>it's on its way</Typography>
+            </Popover>
+        </>;
+    }
+
+
+    function EntryTable({ rows }) {
+        const columns = [
+            { field: 'boat', headerName: 'Boat Name', width: 120, valueGetter: (params) => params.row.data.boat?.name || '' },
+            { field: 'oga_no', headerName: 'OGA No.', width: 80, valueGetter: (params) => params.row.data.boat?.oga_no || '' },
+            // { field: 'skipper', headerName: 'Skipper', width: 100, valueGetter: (params) => params.row.data.user.name },
+            // { field: 'created_at', headerName: 'Submitted', width: 100, valueFormatter: (params) => new Date(params.value).toLocaleDateString() },
+            { field: 'data.rbc', headerName: 'Circumnavigating', width: 100, valueGetter: (params) => params.row.data.rbc || false, valueFormatter: (params) => params.value ? 'Yes' : 'No' },
+            {
+                field: 'port',
+                headerName: 'Ports',
+                width: 400,
+                valueGetter: (params) => params.row.data.port || [],
+                renderCell: renderCellExpandStrings,
+            },
+            {
+                field: 'leg',
+                headerName: 'Crewing Spaces offered per leg',
+                width: 300,
+                valueGetter: (params) => params.row.data.leg || [],
+                renderCell: renderCellExpandObjects,
+            },
+            { field: 'data.ecc', headerName: 'EC Cruise', width: 100, valueGetter: (params) => params.row.data.ecc, valueFormatter: (params) => params.value ? 'Yes' : '' },
+            { field: 'data.email', headerName: 'Contact', renderCell: (params) => <ContactButton params={params} /> },
+        ];
+
+        return (<div style={{ display: 'flex', height: '100%' }}>
+            <div style={{ flexGrow: 1 }}>
+                <DataGrid
+                    rows={rows.map((row, index) => ({ ...row, id: index }))}
+                    columns={columns}
+                    components={{ Toolbar: GridToolbar }}
+                    autoHeight={true}
+                />
+            </div>
+        </div>);
+    }
+
     useEffect(() => {
         const getData = async () => {
-          const p = await getScopedData('member', 'entries', { topic: name }, accessToken);
-          setData(p.data);
+            const p = await getScopedData('member', 'entries', { topic: name }, accessToken);
+            setData(p.data);
         }
         if (accessToken) {
-          getData();
+            getData();
         }
     }, [accessToken, user]);
 
@@ -306,17 +408,18 @@ export default function RBC60Entryies() {
     }
 
     if (!data) {
-        return <CircularProgress/>;
+        return <CircularProgress />;
     }
 
-    const entries =  (data?.Items) || [];
+    const entries = (data?.Items) || [];
 
     const rbc = entries.filter((e) => e.data.rbc);
 
-    const filters = { oga_nos: rbc.map((e) => e.data.boat?.oga_no||'') };
+    const filters = { oga_nos: rbc.map((e) => e.data.boat?.oga_no || '') };
 
     return (
         <Grid container>
+            <LoginButton />
             <Grid item xs={12}>
                 <EntryTable rows={entries} />
             </Grid>
