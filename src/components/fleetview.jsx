d2522349e098ba17b9d4c4f9f9ee795518c57a00
jcable <julian.cable@yahoo.com>
2023-04-06 07:32:48 +0100

    fix: wip

--- src/components/fleetview.jsx
+++ src/components/fleetview.jsx
@@ -15,12 +15,91 @@ import RoleRestricted from './rolerestrictedcomponent';
 import { getFilterable } from './boatregisterposts';
 import { applyFilters, sortAndPaginate } from '../util/oganoutils';
 import { CSVLink } from "react-csv";
+import { gql, useQuery } from '@apollo/client';
+import { getBoatData } from './boatregisterposts';
+import { Button, Dialog } from '@mui/material';
 
-function ExportFleet({name, boats}) {
-  return <CSVLink data={boats}><FileDownloadIcon/></CSVLink>; 
+// thumb			short_description			place_built			construction_details		hull_form	draft	beam	loa	lwl	spar_material	thcf			
+
+async function getBoats(ogaNos) {
+  const r = await Promise.allSettled(ogaNos.map((ogaNo) => getBoatData(ogaNo)));
+  return r.map((b) => b.value.data.result.pageContext.boat);
 }
 
-export function FleetDisplay({ name, filters, tooltip='Click to expand' }) {
+const MEMBER_QUERY = gql(`query members($ids: [Int]!) {
+  members(ids: $ids) {
+    firstname
+    lastname
+    member
+    id
+    GDPR
+  }
+}`);
+
+function id2name(id, members) {
+  const m = members.find((m) => m.id === id);
+  if (m) {
+    return `${m.firstname} ${m.lastname}`;
+  }
+}
+
+function ExportFleet({ name, boats }) {
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const ids = boats.map((b) => b.owners).flat();
+  const membersResult = useQuery(MEMBER_QUERY, { variables: { ids } });
+  const members = membersResult?.data?.members?.filter((m) => m?.GDPR) || [];
+
+  // eslint-disable-next-line no-unused-expressions
+  useEffect(() => {
+    if (open) {
+      getBoats(boats?.map((b) => b?.oga_no))?.then((r) => setData(r))
+    }
+  }, [open, boats]);
+
+  if (!data) {
+    return <Button disabled={true}>Export</Button>
+  }
+  const mb = boats.map((b) => {
+    const boat = { ...b, owners: b?.owners?.map((id) => id2name(id, members)) };
+    const fb = data.find((boat) => boat.oga_no === b.oga_no);
+    if (!fb) {
+      console.log('missing full data for', b);
+      return boat;
+    }
+    const handicap_data = fb.handicap_data || {};
+    console.log(handicap_data);
+    [
+      'short_description', 'hull_form', 'place_built', 'construction_details', 'spar_material',
+    ].forEach((key) => {
+      if (fb[key]) {
+        boat[key] = fb[key];
+      }
+    });
+    [
+      'beam',	'draft',	'length_over_all',	'length_on_waterline',	'thcf',
+    ].forEach((key) => {
+      if (handicap_data[key]) {
+        boat[key] = handicap_data[key];
+      }
+    });
+    return boat;
+  });
+
+  return <RoleRestricted role='member'>
+    <Button onClick={() => setOpen(true)}>Export</Button>
+    <Dialog
+      open={open}
+      aria-labelledby="dialog-update-member-details"
+      maxWidth='md'
+      fullWidth
+    >
+      <CSVLink filename={`${name}.csv`} data={mb}><FileDownloadIcon /></CSVLink>
+    </Dialog>
+  </RoleRestricted>;
+}
+
+export function FleetDisplay({ name, filters, tooltip = 'Click to expand' }) {
   const [page, setPage] = useState(1);
   const [data, setData] = useState();
 
@@ -38,7 +117,7 @@ export function FleetDisplay({ name, filters, tooltip='Click to expand' }) {
   const state = { filters, bpp: 12, page, sort: 'name', sortDirection: 'asc', view: 'app', };
 
   return (<Accordion>
-    <AccordionSummary 
+    <AccordionSummary
       expandIcon={
         <Tooltip placement='left' title={tooltip}>
           <ExpandMoreIcon />
@@ -47,13 +126,13 @@ export function FleetDisplay({ name, filters, tooltip='Click to expand' }) {
       aria-controls="panel1a-content"
       id="panel1a-header"
     >
-        <FleetIcon />
-        <Typography>&nbsp;&nbsp;{name}</Typography>
+      <FleetIcon />
+      <Typography>&nbsp;&nbsp;{name}</Typography>
     </AccordionSummary>
     <AccordionDetails>
       <BoatCards
         state={state} onChangePage={onPageChange} totalCount={filters.oga_nos.length}
-        boats={sortAndPaginate(filtered, state)} otherNav={<ExportFleet name={name} boats={filtered}/>}
+        boats={sortAndPaginate(filtered, state)} otherNav={<ExportFleet name={name} boats={filtered} />}
       />
     </AccordionDetails>
   </Accordion>);
@@ -80,7 +159,7 @@ export function Fleets({ filter }) {
   return (
     <RoleRestricted role='member'>
       {
-        (data?.Items?.map(({ name, filters }) => <FleetDisplay name={name} filters={filters} />))||''
+        (data?.Items?.map(({ name, filters }) => <FleetDisplay name={name} filters={filters} />)) || ''
       }
     </RoleRestricted>
   );
@@ -130,18 +209,18 @@ export function PublicFleetView({ filter }) {
 
   if (data.Items.length > 0) {
     const { filters, name } = data.Items?.[0];
-    return <FleetDisplay name={name} filters={filters} />;  
+    return <FleetDisplay name={name} filters={filters} />;
   }
   return <Typography>No boats to show for fleet defined by {JSON.stringify(filter)}</Typography>;
 }
 
-export default function FleetView({ filter, role='member' }) {
+export default function FleetView({ filter, role = 'member' }) {
   if (role) {
     if (role === 'public') {
-      return <PublicFleetView filter={filter}/>;
+      return <PublicFleetView filter={filter} />;
     }
     return <RoleRestrictedFleetView filter={filter} role={role} />;
   } else {
-    return <PublicFleetView filter={filter}/>;
+    return <PublicFleetView filter={filter} />;
   }
 }
