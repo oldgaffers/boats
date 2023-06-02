6c72deae199a08fe5d076053ee64aa35d3b1cd6e
jcable <julian.cable@bbc.co.uk>
2023-06-02 08:59:29 +0100

    fix: wip

--- src/components/postphotos.jsx
+++ src/components/postphotos.jsx
@@ -1,16 +1,26 @@
 import { getUploadCredentials } from './boatregisterposts';
 import { fromCognitoIdentity } from "@aws-sdk/credential-providers";
 import { S3Client } from "@aws-sdk/client-s3";
-import { Upload } from"@aws-sdk/lib-storage";
+import { Upload } from "@aws-sdk/lib-storage";
+
+function allProgress(proms, progress_cb) {
+    let d = 0;
+    progress_cb(0);
+    return Promise.allSettled(proms.map((p) => {
+        console.log('P', p);
+        return p.then(() => {
+            d++;
+            progress_cb((d * 100) / proms.length);
+        }).catch(err => console.log(err));
+    }));
+}
 
 export async function postPhotos({ email, albumKey, copyright }, fileList) {
     if (fileList?.length > 0) {
         try {
             const { bucketName, region, identityId } = await getUploadCredentials();
-            const client = new S3Client({
-                region,
-                credentials: fromCognitoIdentity({ identityId }),
-            });
+            const credentials = fromCognitoIdentity({ identityId, clientConfig: { region } });
+            const client = new S3Client({ region, credentials });
             const uploads = fileList.map((file) => {
                 const params = {
                     Bucket: bucketName,
@@ -19,9 +29,17 @@ export async function postPhotos({ email, albumKey, copyright }, fileList) {
                     Body: file,
                     Metadata: { albumKey, copyright },
                 };
-                return new Upload({ client, params });
+                const upload = new Upload({ client, params });
+                upload.on("httpUploadProgress", (progress) => {
+                    console.log('progress', progress);
+                });
+                const p = upload.done();
+                return p;
             });
-            return Promise.allSettled(uploads);
+            const a = await allProgress(uploads, (p) => {
+                console.log(`% Done = ${p.toFixed(2)}`);
+            });
+            return a;
         } catch (e) {
             // console.log(e);
         }
