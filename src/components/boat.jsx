35346e8e2c35d54aa22397c5e1f49f8b19fe17b1
jcable <julian.cable@bbc.co.uk>
2023-10-14 08:25:56 +0100

    fix: small improvements

--- src/components/boat.jsx
+++ src/components/boat.jsx
@@ -1,45 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { ApolloConsumer } from '@apollo/client';
-import CircularProgress from "@mui/material/CircularProgress";
 import BoatWrapper from './boatwrapper';
 import { getBoatData } from './boatregisterposts';
-import { Typography } from '@mui/material';
 
 function getOgaNo(location) {
   const params = new URLSearchParams(location.search);
   const qp = params.get('oga_no');
   if (qp) {
-    return qp;
+    return Number(qp);
   }
   const path = location.pathname?.split('/') || ['boat', ''];
   const p = path.indexOf('boat') + 1;
   if (Number(path[p]) !== 0) {
-    return path[p];
+    return Number(path[p]);
   }
-  return undefined;
+  return 0;
 }
 
-export default function Boat({ location }) {
-  const ogaNo = getOgaNo(location);
+function BoatW({ ogaNo }) {
   const [data, setData] = useState();
 
   useEffect(() => {
     if (ogaNo && !data) {
       getBoatData(ogaNo).then((r) => {
-        setData(r);
-      }).catch((e) => console.log(e));
+          setData(r);
+        }).catch((e) => console.log(e));
     }
   }, [data, ogaNo]);
 
-  if (!ogaNo) {
-    return <Typography>Nothing here, go back to the <a href="..">main page</a></Typography>;
+  if (data) {
+    const { boat } = data?.result?.pageContext || { boat: { oga_no: ogaNo, name: '', loading: true } };
+
+    document.title = `${boat.name} (${boat.oga_no})`;
+    return (
+      <ApolloConsumer>
+        {client => <BoatWrapper client={client} location={location} boat={boat} />}
+      </ApolloConsumer>
+    );
   }
+  return '';
+}
 
-  if (!data) return '';//<CircularProgress />;
-
-  if (ogaNo === '') {
+export default function Boat({ location }) {
+  const ogaNo = getOgaNo(location);
+  if (ogaNo === 0) {
     return (<div>
-      <div>You've come to the boat detail viewer but we don't know what boat you wanted.</div>
+      <div data-testid="no-ogano" >You've come to the boat detail viewer but we don't know what boat you wanted.</div>
       <div>If you are looking for boats for sale please visit <a href={location.origin + '/boat_register/boats_for_sale/'}>Boats for Sale</a></div>
       <div>If you are looking interested in trailerable boats please visit <a href={location.origin + '/boat_register/small_boats/'}>Small Boats</a></div>
       <div>To browse our full register please visit <a href={location.origin + '/boat_register/browse_the_register/'}>All Boats</a></div>
@@ -47,13 +53,6 @@ export default function Boat({ location }) {
     </div>);
   }
 
-  const { boat } = data?.result?.pageContext || { boat: { oga_no: ogaNo, name: '', loading: true } };
-
-  document.title = `${boat.name} (${boat.oga_no})`;
-  return (
-    <ApolloConsumer>
-      {client => <BoatWrapper client={client} location={location} boat={boat} />}
-    </ApolloConsumer>
-  );
+  return <BoatW ogaNo={ogaNo} />
 };
 
