1a74d3d23f1f47024bbf15920e137fc3da03d8ae
jcable <boatregister@oga.org.uk>
2025-09-25 10:37:48 +0100

    sorting and filtering

--- src/util/boatsByPlaceBuilt.js
+++ src/util/boatsByPlaceBuilt.js
@@ -2,9 +2,10 @@ import { geolocate, getPlaces } from "./api";
 
 export default async function boatsByPlaceBuilt(filters = {}) {
     // const boats = await getFilterable();
-    const places = await getPlaces();
-    places.sort((a, b) => b.count - a.count);
-    const located = await Promise.all(places.slice(0,700).map(async (p) => {
+    const places = Object.values(await getPlaces());
+    const filtered = places.filter((p) => p.count > 1);
+    filtered.sort((a, b) => b.count - a.count);
+    const located = await Promise.all(filtered.map(async (p) => {
         let n = p.place;
         const g = await geolocate(n);
         if (g.lat) {
