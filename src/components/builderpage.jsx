bf61130794f2310cd1475d798fc1b1f2ed849997
jcable <boatregister@oga.org.uk>
2025-10-02 17:06:17 +0100

    fix: might be string

--- src/components/builderpage.jsx
+++ src/components/builderpage.jsx
@@ -14,41 +14,41 @@ function yards(place) {
 }
 
 function toTitleCase(str) {
-  return str.replace(
-    /\w\S*/g,
-    text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
-  );
+    return str.replace(
+        /\w\S*/g,
+        text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
 }
 
 export function NotableVessels({ notable_vessels }) {
+    if (Array.isArray(notable_vessels) && notable_vessels.length > 0) {
+        return <>
+            <h4>Notable Vessels</h4>
+            <table>
+                <tr><th>Name</th><th>Type</th><th>Period</th><th>Associated With</th><th>Notes</th></tr>
+                {notable_vessels.map((v) => (
+                    <tr><td>{v.name}</td><td>{v.type}</td><td>{v.period}</td><td>{v.associated_with}</td><td>{v.notes || ''}</td></tr>
+                ))}
+            </table>
+        </>;
+    }
     if (!notable_vessels) {
         return '';
     }
-    if (notable_vessels.length === 0) {
-        return '';
-    }
-    return <>
-        <h4>Notable Vessels</h4>
-        <table>
-            <tr><th>Name</th><th>Type</th><th>Period</th><th>Associated With</th><th>Notes</th></tr>
-            {notable_vessels.map((v) => (
-                <tr><td>{v.name}</td><td>{v.type}</td><td>{v.period}</td><td>{v.associated_with}</td><td>{v.notes||''}</td></tr>
-            ))}
-        </table>
-        </>;
+    return notable_vessels;
 }
 
-export function BuilderSummary({ name, place }) {  
-        const [summary, setSummary] = useState();
-        useEffect(() => {
-            const getData = async () => {
-                const data = await getScopedData('public', 'builder', { builder: name, place: place?.place });
-                setSummary(data);
-            }
-            if (!summary) {
-                getData();
-            }
-        }, [summary, name, place]);
+export function BuilderSummary({ name, place }) {
+    const [summary, setSummary] = useState();
+    useEffect(() => {
+        const getData = async () => {
+            const data = await getScopedData('public', 'builder', { builder: name, place: place?.place });
+            setSummary(data);
+        }
+        if (!summary) {
+            getData();
+        }
+    }, [summary, name, place]);
 
     if (!summary) {
         return <div>Loading...</div>;
@@ -73,28 +73,28 @@ export function BuilderSummary({ name, place }) {
         {extra.map((k) => <div key={k}><h4>{toTitleCase(k.replaceAll('_', ' '))}</h4>{JSON.stringify(summary[k])}</div>)}
         <h4>Sources</h4>
         <ul>
-            {(summary.sources||[]).map((s) => (<li key={s}><a href={s}>{s}</a></li>))}
+            {(summary.sources || []).map((s) => (<li key={s}><a href={s}>{s}</a></li>))}
         </ul>
         <div>
-        The above is an AI generated summary of {name}. It could be a load of rubbish and should be checked against other sources.
-        Please contact the Boat Register editors if you find any mistakes.
+            The above is an AI generated summary of {name}. It could be a load of rubbish and should be checked against other sources.
+            Please contact the Boat Register editors if you find any mistakes.
         </div>
     </div>;
 }
 
 export default function BuilderPage({ name, nb }) {
-        const [place, setPlace] = useState();
-       useEffect(() => {
-            const getData = async () => {
-                const places = Object.values(await getPlaces());
-                const filtered = places.filter((p) => yards(p).filter((y) => y.name === name).length > 0);
-                setPlace(filtered[0]);
-            }
-            if (!place) {
-                getData();
-            }
-        }, [place, name]);
-    const nobuilder = (nb||'').split(',').map((o) => Number(o));
+    const [place, setPlace] = useState();
+    useEffect(() => {
+        const getData = async () => {
+            const places = Object.values(await getPlaces());
+            const filtered = places.filter((p) => yards(p).filter((y) => y.name === name).length > 0);
+            setPlace(filtered[0]);
+        }
+        if (!place) {
+            getData();
+        }
+    }, [place, name]);
+    const nobuilder = (nb || '').split(',').map((o) => Number(o));
     console.log('NB', nobuilder);
     return <div>
         <h2>Page for Boat Builder {name}</h2>
@@ -110,6 +110,6 @@ export default function BuilderPage({ name, nb }) {
         <FleetDisplay filters={{ oga_nos: nobuilder }} defaultExpanded={true} />
         If see an error or if you know two or more entries refer to the same builder, please let us know and we will fix / merge them.
         <p></p>
-        <Contact text={name}/>
+        <Contact text={name} />
     </div>;
 }
