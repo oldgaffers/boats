a6dd4f60e5d610c9a00bb55ae9aa91117fe4e4d0
Julian Cable <julian.cable@yahoo.com>
2025-10-04 07:37:17 +0100

    Create boatsbyhomeport.js

---
+++ src/util/boatsbyhomeport.js
@@ -0,0 +1,15 @@
+import { geolocate, getFilterable } from "./api";
+
+export default async function boatsByHomePort(filters = {}) {
+    const boats = await getFilterable()
+    const filtered = boats.filter((b) =>(b.home_port||'') !== '');
+    const located = await Promise.all(filtered.map(async (b) => {
+        const g = await geolocate(b.home_port);
+        if (g?.lat) {
+            return { ...b, ...g, latitude: g.lat, longitude: g.lng };
+        }
+        return null;
+    }));
+    const valid = (await located).filter((p) => p !== null);
+    return { Items: valid };
+}
