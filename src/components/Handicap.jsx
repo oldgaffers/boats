f646c382ecd7354efa7ba59f82580ffebd53b5e4
jcable <boatregister@oga.org.uk>
2025-10-11 17:57:04 +0100

    hide invalid handicaps

--- src/components/Handicap.jsx
+++ src/components/Handicap.jsx
@@ -11,28 +11,35 @@ export function HandicapDisplay({ boat }) {
   const handicapData = boat?.handicap_data || {};
   const thcf = fThcf(boatm2f(boat));
   let handicapDisplay = thcf.toFixed(3);
-	if (thcf > 0.1 && handicapData.last_modified) {
-        const lmd = new Date(handicapData.last_modified);
-        handicapDisplay = `${handicapDisplay} (last modified ${lmd.toLocaleDateString()})`;
+  if (thcf > 0.1) {
+    if (handicapData.last_modified) {
+      const lmd = new Date(handicapData.last_modified);
+      handicapDisplay = `${handicapDisplay} (last modified ${lmd.toLocaleDateString()})`;
     }
     if (handicapData.thcf && Math.abs(thcf - handicapData.thcf) > 0.01) {
       handicapDisplay = `${handicapDisplay} (stored THCF ${handicapData.thcf.toFixed(3)})`;
     }
     if (hideUnlessChecked && (handicapData.checked === false)) {
       return <>
-       <Typography variant='subtitle2' component='span'>T(H)CF: </Typography>
-       <Typography variant="body1" component='span'>
-        We are asking all boat owners to re-validate the data used to calculate handicaps.
-        The best way to do this is to use the 'I have Edits' button and step through the choices,
-        making any changes you want. If all is correct, just submit the form. Alternatively, email
-        the boat register editors.
-       </Typography>
+        <Typography variant='subtitle2' component='span'>T(H)CF: </Typography>
+        <Typography variant="body1" component='span'>
+          We are asking all boat owners to re-validate the data used to calculate handicaps.
+          The best way to do this is to use the 'I have Edits' button and step through the choices,
+          making any changes you want. If all is correct, just submit the form. Alternatively, email
+          the boat register editors.
+        </Typography>
       </>;
     }
     return <>
       <ConditionalText label='T(H)CF' value={handicapDisplay} />
       <ConditionalText label='Solent Rating' value={handicapData.solent?.thcf?.toFixed(3)} />
     </>;
+  } else if (handicapData.thcf) {
+    const val = handicapData.thcf.toFixed(3);
+    return <Typography>Handicap cannot be calculated but we do have a stored value of {val}. This should only be used with caution.</Typography>;
+  } else {
+    return '';
+  }
 }
 
 export const solentSteps = (thisStep, nextStep) => {
@@ -177,7 +184,7 @@ export const solentSteps = (thisStep, nextStep) => {
           isReadOnly: true,
           resolveProps: (props, { meta, input }, formOptions) => {
             const { values } = formOptions.getState();
-            const displacement = values?.handicap_data?.displacement;            
+            const displacement = values?.handicap_data?.displacement;
             if (displacement && !Number.isNaN(displacement)) {
               formOptions.change("ddf.effective_displacement", displacement);
               return {
@@ -204,7 +211,7 @@ export const solentSteps = (thisStep, nextStep) => {
             const { values } = formOptions.getState();
             const value = values?.handicap_data?.solent?.performance_factor || 0;
             return {
-              value: `${100*value}%`,
+              value: `${100 * value}%`,
             }
           },
         },
@@ -315,7 +322,7 @@ export const solentSteps = (thisStep, nextStep) => {
             };
           },
         },
-        
+
       ],
     },
   ];
@@ -526,7 +533,7 @@ export const steps = (firstStep, nextStep) => [
     nextStep: ({ values }) => {
       if (values.ddf.use_sailarea) {
         return "handicap-hull-step";
-      } 
+      }
       if (['Single Sail', 'Cat Boat'].includes(values.rig_type)) {
         return "mainsail-step";
       }
