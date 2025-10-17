14344d038a4e01f65272efb54ebb699fb1331ffe
jcable <boatregister@oga.org.uk>
2025-10-17 10:48:29 +0100

    wip

--- src/components/postphotos.jsx
+++ src/components/postphotos.jsx
@@ -5,33 +5,41 @@ import { Upload } from "@aws-sdk/lib-storage";
 
 export async function postPhotos(copyright, email, albumKey, fileList, onProgress = () => { }) {
     const { bucketName, region, identityId } = await getUploadCredentials();
-    const credentials = fromCognitoIdentity({ identityId, clientConfig: { region } });
-    console.log('Upload credentials', bucketName, region, credentials);
-    const client = new S3Client({ region, credentials });
+    console.log('Upload credentials', bucketName, region, identityId);
+    const client = new S3Client({ region, credentials: fromCognitoIdentity({ identityId, clientConfig: { region } }) });
     console.log('S3 Client', client);
     const progress = fileList.reduce((acc, file) => ({ ...acc, [file.name]: { loaded: 0, total: file.size } }), {});
     const totalSize = fileList.reduce((acc, file) => acc + file.size, 0);
     const uploaders = fileList.map((file) => {
-        const upload = new Upload({
-            client, params: {
-                Bucket: bucketName,
-                Key: `${email}/${file.name}`,
-                ContentType: file.type,
-                Body: file,
-                Metadata: { albumKey, copyright },
+        try {
+            const upload = new Upload({
+                client, params: {
+                    Bucket: bucketName,
+                    Key: `${email}/${file.name}`,
+                    ContentType: file.type,
+                    Body: file,
+                    Metadata: { albumKey, copyright },
+                }
+            });
+            console.log('Starting upload for', file.name);
+            console.log('Upload object', upload);
+            upload.on("httpUploadProgress", (p) => {
+                progress[file.name].loaded = p.loaded;
+                const loaded = Object.values(progress).reduce((acc, v) => acc + v.loaded, 0);
+                onProgress(Math.round((loaded / totalSize) * 100));
+            });
+            try {
+                return upload.done();
+            } catch (e) {
+                console.error('Error in upload done for', file.name, e);
             }
-        });
-        console.log('Starting upload for', file.name);
-        console.log('Upload object', upload);
-        upload.on("httpUploadProgress", (p) => {
-            progress[file.name].loaded = p.loaded;
-            const loaded = Object.values(progress).reduce((acc, v) => acc + v.loaded, 0);
-            onProgress(Math.round((loaded / totalSize) * 100));
-        });
-        return upload.done();
+        } catch (e) {
+            console.error('Error creating upload for', file.name, e);
+        }
+        return null;
     });
     console.log('Uploading', uploaders);
     const p = Promise.allSettled(uploaders)
     console.log('Upload promises', p);
-    return p;;
+    return p;
 }
