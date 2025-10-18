20629e2dac7d6230183ec2d73dc90ba36f17dce9
jcable <boatregister@oga.org.uk>
2025-10-18 15:23:42 +0100

    files to be used by vite tooling

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import 'react-app-polyfill/ie11';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import './index.css'
+import { BoatRegister } from './app.jsx'
+
+// convert paragraph elements with <<xxxx>> to divs so that we can nest p and divs inside them
+// we need this because most editors don't have html access.
+function handleParagraphs() {
+  const allparas = document.getElementsByTagName('p');
+  for (let i = 0; i < allparas.length; i++) {
+    const p = allparas.item(i);
+    const text = p.innerText;
+    const q = text.match(/^<<(.*?):(.*)>>$/);
+    if (q?.length === 3) {
+      const [, component, arglist] = q;
+      const args = arglist.split(':');
+      const al = args.map((a, index) => `data-oga-arg${index}="${a.trim()}"`).join(' ');
+      p.outerHTML = `<div data-oga-component=${component} ${al}></div>`;
+    }
+  }
+}
+
+handleParagraphs();
+const placeholders = document.querySelectorAll("[data-oga-component]");
+placeholders.forEach((ph) => {
+  const attr = ph.dataset;
+  createRoot(ph).render(<BoatRegister {...attr} />);
+});
