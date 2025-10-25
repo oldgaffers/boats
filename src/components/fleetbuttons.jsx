a7c249a08e48383d9541a39e1e775a4c5152105a
jcable <boatregister@oga.org.uk>
2025-10-25 16:37:20 +0100

    remove unused

--- src/components/fleetbuttons.jsx
+++ src/components/fleetbuttons.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Picker from './picker';
