ba4e7311fa4ed82b31f612812de721fd3bad5b89
Julian Cable <julian.cable@yahoo.com>
2025-10-30 11:24:43 +0000

    fix: tests passing

--- tests/browser/boat.test.jsx
+++ tests/browser/boat.test.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { test, expect, vi } from 'vitest';
-import { screen, render } from 'vitest-browser-react';
-import Boat from './boat';
-import * as api from '../util/api';
+import { render } from 'vitest-browser-react';
+import Boat from '../../src/components/boat';
+import * as api from '../../src/util/api';
 
-vi.mock("../util/api", () => {
+vi.mock("../../src/util/api", () => {
   return {
     getBoatData: () => Promise.resolve({ name: 'x' }),
     getPicklists: () => Promise.resolve({}),
+    getScopedData: () => Promise.resolve(vi.fn()),
+    postScopedData: () => Promise.resolve(vi.fn()),
+    postGeneralEnquiry: () => Promise.resolve(vi.fn()),
+    getUploadCredentials: () => Promise.resolve(vi.fn()),
+    createPhotoAlbum: () => Promise.resolve(vi.fn()),
+    getAlbumKey: () => Promise.resolve(vi.fn()),
+    postBoatData: () => Promise.resolve(vi.fn()),
+    getBoatLastModified: () => Promise.resolve(vi.fn()),
+    nextOgaNo: () => Promise.resolve(vi.fn()),
   };
 });
 
-/*
 test('renders boat', async () => {
-  render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <Boat ogaNo={1}/>
-    </MockedProvider>
+  const screen = await render(
+    <Boat location={{ search: '?oga_no=1' }} ogaNo={1} />
   );
-  await waitFor(() => {
-    const buttons = screen.getAllByRole('button');
-    expect(buttons.length).toBe(5);
-  });
-
+  expect(screen.getByRole('button').length).toBe(6);
 });
-*/
+
 test('renders missing ogano', async () => {
-  render(<Boat ogaNo={0}/>);
-  expect(screen.getAllByRole('link').length).toBe(4);
+  const screen = await render(<Boat location={{ search: '?oga_no=0' }} ogaNo={0} />);
+  expect(screen.getByRole('link').length).toBe(4);
 
 });
