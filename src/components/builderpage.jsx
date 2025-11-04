fcb60118ea3dfcab072dc4559c9bf8dd05b583df
Julian Cable <julian.cable@yahoo.com>
2025-11-04 22:47:58 +0000

    buttons

--- src/components/builderpage.jsx
+++ src/components/builderpage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { getPlaces, getScopedData } from '../util/api';
 import { FleetDisplay } from './fleetview';
 import Contact from './contact';
+import MergeButton from './mergebutton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function toTitleCase(str) {
     return str.replace(
@@ -15,26 +17,26 @@ export function VesselTable({ heading, vessels }) {
     if (Array.isArray(vessels)) {
         if (vessels.length === 0)
             return '';
-    return <>
-        <h4>{title}</h4>
-        <table>
-            <thead>
-            <tr><th>Name</th><th>Type</th><th>Period</th><th>Associated With</th><th>Notes</th></tr>
-            </thead>
-            <tbody>
-            {vessels.map((v, index) => (
-                <tr key={index}><td>{v.name}</td><td>{v.type}</td><td>{v.period}</td><td>{v.associated_with}</td><td>{v.notes || ''}</td></tr>
-            ))}
-            </tbody>
-        </table>
-    </>;
+        return <>
+            <h4>{title}</h4>
+            <table>
+                <thead>
+                    <tr><th>Name</th><th>Type</th><th>Period</th><th>Associated With</th><th>Notes</th></tr>
+                </thead>
+                <tbody>
+                    {vessels.map((v, index) => (
+                        <tr key={index}><td>{v.name}</td><td>{v.type}</td><td>{v.period}</td><td>{v.associated_with}</td><td>{v.notes || ''}</td></tr>
+                    ))}
+                </tbody>
+            </table>
+        </>;
     }
     return <div><h4>{title}</h4><div>{vessels}</div></div>
 }
 
 export function NotableVessels({ notable_vessels }) {
     if (Array.isArray(notable_vessels) && notable_vessels.length > 0) {
-        return <VesselTable heading='Notable Vessels' vessels={notable_vessels}/>
+        return <VesselTable heading='Notable Vessels' vessels={notable_vessels} />
     }
     if (!notable_vessels) {
         return '';
@@ -43,7 +45,7 @@ export function NotableVessels({ notable_vessels }) {
         return notable_vessels;
     }
     const categories = Object.keys(notable_vessels);
-    return <>{categories.map((c) => <VesselTable key={c} heading={c} vessels={notable_vessels[c]}/>)}</>;
+    return <>{categories.map((c) => <VesselTable key={c} heading={c} vessels={notable_vessels[c]} />)}</>;
 }
 
 export function BuilderSummary({ name, place }) {
@@ -90,7 +92,7 @@ export function BuilderSummary({ name, place }) {
     </div>;
 }
 
-export function AllBuilders({ place, yards, yard }) {
+export function AllBuilders({ email, place, yards, yard }) {
     const [merge, setMerge] = useState([]);
     const [keep, setKeep] = useState();
 
@@ -114,23 +116,24 @@ export function AllBuilders({ place, yards, yard }) {
         <h3>All builders referenced in the OGA Boat Register in {place}</h3>
         <FormControl>
             <RadioGroup defaultValue={yard} onChange={handleKeep}>
-                <Box sx={{ display: 'grid', gridTemplateColumns: '2fr 1fr 1fr'}}>
-                <Box></Box><Box>Merge</Box><Box>Keep</Box>
-                    {yards.map(({name, count}) =>
+                <Box sx={{ display: 'grid', gridTemplateColumns: '2fr 1fr 1fr' }}>
+                    <Box></Box><Box>Merge</Box><Box>Keep</Box>
+                    {yards.map(({ name, count }) =>
                         <>
-                                {
-                                    (name === yard)
-                                        ?
-                                        <span>{name} {count}</span>
-                                        :
-                                        <a href={`/boat_register/builder/?name=${name}&place=${place}`}>{name} {count}</a>
-                                }
-                                <Checkbox value={name} onChange={(ev) => handleMerge(name, ev.target.checked)}/>
-                                <Radio value={name}/>
+                            {
+                                (name === yard)
+                                    ?
+                                    <span>{name} {count}</span>
+                                    :
+                                    <a href={`/boat_register/builder/?name=${name}&place=${place}`}>{name} {count}</a>
+                            }
+                            <Checkbox value={name} onChange={(ev) => handleMerge(name, ev.target.checked)} />
+                            <Radio value={name} />
                         </>
                     )}
                 </Box>
             </RadioGroup>
+            <MergeButton update={{ email, keep, merge, field: 'builder', id: `${place}_${new Date().toISOString()}` }} />
         </FormControl>
     </>;
 }
@@ -159,6 +162,7 @@ export function NoBuilder({ place, boats }) {
 
 export default function BuilderPage({ name, place }) {
     const [location, setLocation] = useState();
+    const { user, isAuthenticated } = useAuth0();
     useEffect(() => {
         const getData = async () => {
             const places = await getPlaces();
@@ -169,7 +173,7 @@ export default function BuilderPage({ name, place }) {
                 if (p) {
                     setLocation(p);
                 } else {
-                    setLocation({ place: 'unspecified place', yards: []});
+                    setLocation({ place: 'unspecified place', yards: [] });
                 }
             }
         }
@@ -190,7 +194,13 @@ export default function BuilderPage({ name, place }) {
         <p></p>
         <h3>Boats built by {name} according to OGA Boat Register data</h3>
         <FleetDisplay filters={{ builder: name }} defaultExpanded={true} />
-        <OtherBuilders place={location.place} yard={name} yards={yards} />
+        {
+            (isAuthenticated)
+            ?
+                <AllBuilders email={user?.email} place={location.place} yard={name} yards={yards} />
+            :      
+                <OtherBuilders place={location.place} yard={name} yards={yards} />
+        }
         <NoBuilder place={location.place} boats={location.no_yard} />
         If see an error or if you know two or more entries refer to the same builder, please let us know and we will fix / merge them.
         <p></p>
