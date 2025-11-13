6010b2aa5db86f5a6e393220a0bd41d3f18d5042
Julian Cable <julian.cable@yahoo.com>
2025-11-13 21:45:20 +0000

    Update skippers.jsx

--- src/components/skippers.jsx
+++ src/components/skippers.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Paper } from "@mui/material";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
 export default function Skippers({skippers, email}) {
     return <Box>
