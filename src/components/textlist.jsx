d230c0b5844d2553aadb17ff7b9e779c5389bc95
Julian Cable <julian.cable@yahoo.com>
2025-11-13 22:00:10 +0000

    Update textlist.jsx

--- src/components/textlist.jsx
+++ src/components/textlist.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material';
+import Box from '@mui/material/Box';
 
 export default function TextList({ variant='body2', fields, data }) {
     const r = [];
