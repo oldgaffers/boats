83e33669cdfe04f71923c04306d7ebc0be344458
Julian Cable <julian.cable@yahoo.com>
2025-11-15 18:55:36 +0000

    Update sortchoices.jsx

--- src/components/sortchoices.jsx
+++ src/components/sortchoices.jsx
@@ -33,87 +33,20 @@ function makePicklist(view, pickers, field) {
   return [];
 }
 
-export default function SearchAndFilterBoats({
+export default function SortingAndPagination({
   sortDirection,
   sortField,
   boatsPerPage,
-  filters,
   view,
-  pickers,
-  onFilterChange,
   onPageSizeChange = () => console.log('onPageSizeChange'),
   onSortChange = () => console.log('onSortChange'),
-  onMarkedOnlyChange = (v) => console.log('onMarkedOnly', v),
-  isMarkedOnly,
-  onClearAllMarks=() => console.log('clear all marks'),
-  onOwnedOnlyChange = (v) => console.log('onOwnedOnly', v),
-  isOwnedOnly,
-  enableOwnersOnly = false,
-  filtered,
-  fleets,
-  fleetName,
 }) {
-  const currentFilters = filters || {};
-  const [ogaNo, setOgaNo] = useState(currentFilters.oga_no || '');
+
   const debouncedOgaNo = useDebounce(ogaNo, 1000);
-  const markList = useContext(MarkContext);
-
-  useEffect(() => {
-    if (debouncedOgaNo !== '') {
-      // console.log("use effect ogaNo, debounced oga no", ogaNo, debouncedOgaNo);
-      if (ogaNo === "") {
-        const { oga_no, ...f } = filters;
-        onFilterChange(f);
-      } else if (filters?.oga_no) {
-        // console.log("new", debouncedOgaNo, "old", filters.oga_no);
-        const newNo = debouncedOgaNo;
-        if (newNo !== filters.oga_no) {
-          onFilterChange({ ...filters, oga_no: newNo });
-        }
-      } else {
-        onFilterChange({ ...filters, oga_no: debouncedOgaNo });
-      }
-    }
-  }, [debouncedOgaNo, filters, onFilterChange, ogaNo]);
-
-  const dateRange = [
-    currentFilters.firstYear || pickers.year.min,
-    currentFilters.lastYear || pickers.year.max,
-  ];
-  const [dr, setDr] = useState(dateRange);
 
   const handlePageSizeChange = (_, bpp) => {
     onPageSizeChange(parseInt(bpp, 10));
   };
-
-  function pl(id, value) {
-    if (value) {
-      onFilterChange({ ...currentFilters, [id]: value });
-    } else {
-      const f = { ...currentFilters };
-      delete f[id];
-      onFilterChange(f);
-    }
-  }
-
-  function handleDateRange(event, newValue) {
-    setDr(newValue);
-  }
-
-  function handleDateRangeCommitted(event, [min, max]) {
-    const f = { ...currentFilters };
-    if (min === pickers.year.min) {
-      delete f.firstYear;
-    } else {
-      f.firstYear = min;
-    }
-    if (max === pickers.year.max) {
-      delete f.lastYear;
-    } else {
-      f.lastYear = max;
-    }
-    onFilterChange(f);
-  }
 
   const sortOptions = [
     { field: "name", name: "Name", direction: "asc" },
@@ -151,20 +84,8 @@ export default function SearchAndFilterBoats({
     }
   }
 
-  function fleetsUpdated() {
-    console.log('fleets updated');
-  }
-
-  function filterByFleet(name) {
-    if (name) {
-      onFilterChange(fleets.find((f) => f.name === name).filters, name);
-    } else {
-      onFilterChange({});
-    }
-  }
-
   return (
-    <form>
+    <>
       <FormHelperText sx={{ marginLeft: "1em", marginBottom: "3px" }}>
         Use these controls to sort the list by name, price, etc. and to choose
         how much you want to see
@@ -223,170 +144,6 @@ export default function SearchAndFilterBoats({
           </FormControl>
         </Grid>
       </Grid>
-      <Divider />
-      <FormHelperText sx={{ marginLeft: "1em", marginBottom: "3px" }}>
-        Use these controls to filter the list in one or more ways
-      </FormHelperText>
-      <Grid
-        container
-        direction="row"
-        justify="space-evenly"
-        alignItems="center"
-      >
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="name"
-            options={makePicklist(view, pickers, "name")}
-            label="Boat Name"
-            value={currentFilters["name"]}
-          />
-        </Grid>
-        <Grid item>
-          <NumberEntry
-            id="oga_no"
-            label="OGA Boat No."
-            value={ogaNo}
-            onSet={setOgaNo}
-            onClear={() => { setOgaNo(''); }}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="designer"
-            options={makePicklist(view, pickers, "designer")}
-            label="Designer"
-            value={currentFilters["designer"]}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="builder"
-            options={makePicklist(view, pickers, "builder")}
-            label="Builder"
-            value={currentFilters["builder"]}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="rig_type"
-            options={makePicklist(view, pickers, "rig_type")}
-            label="Rig Type"
-            value={currentFilters["rig_type"]}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="mainsail_type"
-            options={makePicklist(view, pickers, "mainsail_type")}
-            label="Mainsail Type"
-            value={currentFilters["mainsail_type"]}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="generic_type"
-            options={makePicklist(view, pickers, "generic_type")}
-            label="Generic Type"
-            value={() => {
-              const f = currentFilters["generic_type"];
-              if (Array.isArray(f)) {
-                return ""; // TODO support multiple
-              }
-              return f;
-            }}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="design_class"
-            options={makePicklist(view, pickers, "design_class")}
-            label="Design Class"
-            value={currentFilters["design_class"]}
-          />
-        </Grid>
-        <Grid item>
-          <Picker
-            onChange={pl}
-            id="construction_material"
-            options={makePicklist(view, pickers, "construction_material")}
-            label="Construction Material"
-            value={currentFilters["construction_material"]}
-          />
-        </Grid>
-        <Grid item>
-          <DateRangePicker
-            value={dr}
-            yearProps={pickers.year}
-            label={`Built Between: ${dateRange[0]} and ${dateRange[1]}`}
-            onChange={handleDateRange}
-            onChangeCommitted={handleDateRangeCommitted}
-            min={pickers.year.min}
-            max={pickers.year.max}
-            step={pickers.year.step}
-          />
-        </Grid>
-        <Grid item>
-          <FormControlLabel
-            disabled={markList.length === 0}
-            id="marked"
-            onChange={(event) => onMarkedOnlyChange(event.target.checked)}
-            control={<Switch checked={isMarkedOnly} />}
-            label="Only Marked Boats"
-          />
-        </Grid>
-          <Button
-            disabled={markList.length === 0}
-            onClick={onClearAllMarks}
-            variant='contained' size='small' color='secondary'
-          >Clear Marks</Button>
-        <Grid item>
-        </Grid>
-        <Grid item>
-          <RoleRestricted role='member'>
-            <FleetButtons
-              onSelectionChange={filterByFleet}
-              onFleetsUpdated={fleetsUpdated}
-              filters={filters}
-              filtered={filtered}
-              fleets={fleets}
-              fleetName={fleetName}
-            />
-          </RoleRestricted>
-        </Grid>
-        <RoleRestricted role='member'>
-          <Grid item>
-            <FormControlLabel
-              disabled={!enableOwnersOnly}
-              id="owned"
-              onChange={(event) => onOwnedOnlyChange(event.target.checked)}
-              control={<Switch sx={{ marginLeft: '30px' }} checked={!!isOwnedOnly} />}
-              label="Only My Boats"
-            />
-            </Grid>
-            {
-              (enableOwnersOnly) ?
-                ''
-                :
-                <Grid item padding={'1px'} margin='1px'>
-                <Typography>
-                  We don't have a record of you owning any boats.
-                  </Typography><Typography>
-                  Tell us about a boat you own by clicking
-                  on the boat's More button.
-                  </Typography><Typography>
-                  If your boat isn't on the register, click the add boat button.
-                </Typography>
-                </Grid>
-            }
-        </RoleRestricted>
-      </Grid>
-    </form>
+    </>
   );
 }
