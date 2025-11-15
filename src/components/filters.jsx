e0e0bcf78c3c9d83f83ec7d486e18700dfb3a689
Julian Cable <julian.cable@yahoo.com>
2025-11-15 18:59:40 +0000

    Update filters.jsx

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -33,16 +33,11 @@ function makePicklist(view, pickers, field) {
   return [];
 }
 
-export default function SearchAndFilterBoats({
-  sortDirection,
-  sortField,
-  boatsPerPage,
+export default function FilterBoats({
   filters,
   view,
   pickers,
   onFilterChange,
-  onPageSizeChange = () => console.log('onPageSizeChange'),
-  onSortChange = () => console.log('onSortChange'),
   onMarkedOnlyChange = (v) => console.log('onMarkedOnly', v),
   isMarkedOnly,
   onClearAllMarks=() => console.log('clear all marks'),
@@ -82,10 +77,6 @@ export default function SearchAndFilterBoats({
   ];
   const [dr, setDr] = useState(dateRange);
 
-  const handlePageSizeChange = (_, bpp) => {
-    onPageSizeChange(parseInt(bpp, 10));
-  };
-
   function pl(id, value) {
     if (value) {
       onFilterChange({ ...currentFilters, [id]: value });
@@ -115,42 +106,6 @@ export default function SearchAndFilterBoats({
     onFilterChange(f);
   }
 
-  const sortOptions = [
-    { field: "name", name: "Name", direction: "asc" },
-    { field: "oga_no", name: "OGA No.", direction: "asc" },
-    { field: "year", name: "Age", direction: "asc" },
-    { field: "updated_at", name: "Updated", direction: "desc" },
-    { field: "length_on_deck", name: "Length", direction: "desc" },
-    { field: "rank", name: "Editor's choice", direction: "asc" },
-  ];
-  if (view === 'sell') {
-    sortOptions.push({ field: "price", name: "Price", direction: "desc" });
-  }
-  const sortLabelByField = sortOptions.reduce((r, { field, name }) => {
-    r[field] = name;
-    return r;
-  }, {});
-  const sortDirectionByField = sortOptions.reduce((r, { field, direction }) => {
-    r[field] = direction;
-    return r;
-  }, {});
-
-  function handleSortFieldChange(event) {
-    const field = event.target.value;
-    if (field !== sortField) {
-      const normal = sortDirectionByField[field];
-      onSortChange(field, normal);
-    }
-  }
-
-  function handleSortDirectionChange(event) {
-    const normal = sortDirectionByField[sortField];
-    const dir = event.target.checked ? opposite[normal] : normal;
-    if (dir !== sortDirection) {
-      onSortChange(sortField, dir);
-    }
-  }
-
   function fleetsUpdated() {
     console.log('fleets updated');
   }
@@ -164,66 +119,7 @@ export default function SearchAndFilterBoats({
   }
 
   return (
-    <form>
-      <FormHelperText sx={{ marginLeft: "1em", marginBottom: "3px" }}>
-        Use these controls to sort the list by name, price, etc. and to choose
-        how much you want to see
-      </FormHelperText>
-      <Grid
-        container
-        direction="row"
-        justify="space-evenly"
-        alignItems="flex-end"
-      >
-        <Grid item>
-          <Picker
-            clearable={false}
-            value={`${boatsPerPage}`}
-            id="page-size"
-            onChange={handlePageSizeChange}
-            options={pageSize}
-            label="Boats Per Page"
-          />
-        </Grid>
-        <Grid item>
-          <FormControl sx={{ marginLeft: "1.5em" }}>
-            <FormLabel>Sort By</FormLabel>
-            <RadioGroup
-              row
-              aria-label="sorting"
-              name="sorting"
-              value={sortLabelByField[sortField]}
-              onChange={handleSortFieldChange}
-            >
-              {sortOptions.map((option) => (
-                <FormControlLabel
-                  key={option.name}
-                  value={option.field}
-                  sx={{ marginRight: "1em", borderRightWidth: "1vw" }}
-                  control={<Radio checked={sortField === option.field} />}
-                  label={option.name}
-                />
-              ))}
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item>
-          <FormControl sx={{ marginLeft: "1.5em" }}>
-            <FormLabel>Sort Direction</FormLabel>
-            <FormControlLabel
-              id="sort-direction"
-              onChange={handleSortDirectionChange}
-              control={
-                <Switch
-                  checked={sortDirection !== sortDirectionByField[sortField]}
-                />
-              }
-              label="reversed"
-            />
-          </FormControl>
-        </Grid>
-      </Grid>
-      <Divider />
+    <>
       <FormHelperText sx={{ marginLeft: "1em", marginBottom: "3px" }}>
         Use these controls to filter the list in one or more ways
       </FormHelperText>
@@ -387,6 +283,6 @@ export default function SearchAndFilterBoats({
             }
         </RoleRestricted>
       </Grid>
-    </form>
+    </>
   );
 }
