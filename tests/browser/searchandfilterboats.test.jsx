715efdf0cdece61886f01ba795b06fc0d321c47f
Julian Cable <julian.cable@yahoo.com>
2025-11-15 21:44:30 +0000

    Update searchandfilterboats.test.jsx

--- tests/browser/searchandfilterboats.test.jsx
+++ tests/browser/searchandfilterboats.test.jsx
@@ -19,5 +19,5 @@ test('renders learn react link', async () => {
       />
   );
   const wanted = getByText(/Boat Name/);
-  expect(wanted.length).toBeGreaterThanOrEqual();
+  expect(wanted.items().length).toBeGreaterThanOrEqual();
 });
