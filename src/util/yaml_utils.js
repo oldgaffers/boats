83b03591997f6408e9adb186a14f1ba29bff8552
jcable <boatregister@oga.org.uk>
2025-11-26 19:59:42 +0000

    fix: sales text in PR

--- src/util/yaml_utils.js
+++ src/util/yaml_utils.js
@@ -1,13 +1,13 @@
 import { parse } from 'yaml';
-import Showdown from "showdown";
+import * as showdown from "showdown";
 
 export async function getBoatFromYAML(yaml) {
     const p = parse(yaml);
-    const converter = new Showdown.Converter();
+    const converter = new showdown.Converter();
     p.short_description = converter.makeHtml(p.short_description);
     p.full_description = converter.makeHtml(p.full_description);
     (p?.for_sales || []).forEach((s) => {
-      s.sales_text = converter(s.sales_text);
+      s.sales_text = converter.makeHtml(s.sales_text);
     });
     return p;
 }
