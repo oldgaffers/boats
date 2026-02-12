66379829202ec9d7b5b0b58a43adc44f4397214c
Julian Cable <julian.cable@yahoo.com>
2026-02-12 11:59:34 +0000

    Update editbutton.jsx

--- src/components/editbutton.jsx
+++ src/components/editbutton.jsx
@@ -2,14 +2,30 @@ import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import Snackbar from '@mui/material/Snackbar';
-import { postBoatData } from '../util/api';
+import { createPhotoAlbum, postBoatData } from '../util/api';
 import EditBoatWizard from './editboatwizard';
 import { boatf2m, boatm2f } from '../util/format';
 import { boatdiff } from './editboatwizardfunctions';
 import { formatters } from 'jsondiffpatch';
 
 
-
+  
+async function checkAlbum(boat) {
+   if (boat.image_key) {
+     return boat;
+   }
+   const r = await createPhotoAlbum(boat.name, no);
+   if (r.ok) {
+      const j = await r.json();
+      return {...boat, image_key: j.albumKey };
+   } else {
+      console.log('problem creating album for new boat', r.status, r.statusText);
+      const j = await r.json();
+      if (j.albumKey) {
+         return {...boat, image_key: j.albumKey };
+      }
+   }
+}
 
 export default function EditButton({ boat, label = 'I have edits for this boat' }) {
   const [open, setOpen] = useState(false);
@@ -19,7 +35,8 @@ export default function EditButton({ boat, label = 'I have edits for this boat' 
 
   async function submitBoatEdits(email, changes, newItems, updated) {
     try {
-      const response = await postBoatData({ email, changes, newItems, new: updated });
+      const boat = await checkAlbum(updated);
+      const response = await postBoatData({ email, changes, newItems, new: boat });
       if (response.ok) {
         // console.log('submitted');
         setSnackBarText("Thanks, we'll get back to you.");
