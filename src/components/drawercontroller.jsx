c3774915ec7f3fd69beed6c5dbc9d05ebc7b2054
Julian Cable <julian.cable@yahoo.com>
2026-06-18 09:42:57 +0100

    Refactor DrawerController to remove styles
    Removed unused styles and simplified the DrawerController component.

--- src/components/drawercontroller.jsx
+++ src/components/drawercontroller.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Icon from '@mui/material/Icon';
-import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
-      },
-    },
-  }));
-  
-  function DrawerController({ onClick }) {
-    const classes = useStyles();
+export default function DrawerController({ onClick }) {
     return (
         <IconButton
         color="inherit"
         aria-label="open drawer"
         edge="start"
         onClick={onClick}
-        className={classes.menuButton}
         >
         <Icon>menu</Icon>
         </IconButton>
     );
-  }
-export default DrawerController
+}
